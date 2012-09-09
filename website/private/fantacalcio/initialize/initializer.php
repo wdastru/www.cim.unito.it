e592ffd4d5e6da2623c558c3f40f8935543dc940
Walter Dastru <walter.dastru@gmail.com>
2012-09-09 07:51:28 +0200

    creato initializer - rimozione offerte

--- website/private/fantacalcio/initialize/initializer.php
+++ website/private/fantacalcio/initialize/initializer.php
@@ -1,7 +1,11 @@
 <?php
 $relocate_string = "../";
 
-
+/*
+ * elimina offerte
+ * * * * * * * * */
+system("rm " . $relocate_string . "777//offerte//offerta*.txt");
+system("rm " . $relocate_string . "777//offerte//archive//offerta*.txt");
 
 $where = "Location: " . $relocate_string . "initialize/index.php";
 header($where); 
