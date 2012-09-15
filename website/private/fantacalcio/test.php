e8ad43fc30b5764fddbc038e0fc1ca33f899e7fe
Walter Dastru <walter.dastru@gmail.com>
2012-09-15 07:24:02 +0200

    created  website/private/fantacalcio/test.php for upload of files

---
+++ website/private/fantacalcio/test.php
@@ -0,0 +1,10 @@
+<?php
+	$file_location = $_FILES['uploaded_file']['tmp_name'];	
+	$dir_file = "777/" . basename($_FILES['uploaded_file']['name']);
+	if(move_uploaded_file($file_location,"$dir_file")) {
+    	echo "File was uploaded";
+	}
+	else{
+    	echo "File was not uploaded";
+	}	
+?>
