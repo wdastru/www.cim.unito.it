fe17375df504cbc608e49a37b42b783b4b0fc2e3
Walter Dastru <walter.dastru@gmail.com>
2012-10-13 10:18:40 +0200

    added messages

--- website/private/fantacalcio/include/banner_slider.inc.php
+++ website/private/fantacalcio/include/banner_slider.inc.php
@@ -1,10 +1,33 @@
+
 <div class="marquee" id="mycrawler">
 	<span style="color: #A9FA05;">
 	<?php
-		for ($i=0; $i < 150; $i++) { 
+			for ($i=0; $i < 150; $i++) { 
 			echo "-";
 		}
 	?>
 	</span>
-	Those confounded friars dully buzz that faltering jay. An appraising tongue acutely causes our courageous hogs. Their fitting submarines deftly break your approving improvisations. Her downcast taxonomies actually box up those disgusted turtles.
+	<?php
+		$messagesXml = "messages.xml";
+		if(file_exists($messagesXml))
+		{
+			//read the contents into a string
+			$str = file_get_contents($messagesXml,"rb");
+			$myparser = xml_parser_create();
+			xml_parse_into_struct($myparser, $str, $text);
+			xml_parser_free($myparser);
+			//print_r($text);
+			
+			$j = 0;
+			for($i=0; $i<count($text); ++$i) {
+				if ($text[$i]['tag'] == 'MESSAGE') {
+					echo $text[$i]['value'] . " --- ";
+				}
+			}
+		} else {
+			/*
+			 * TODO completare ?
+			 * * * * * * * * * */
+		}
+	?>
 </div>
