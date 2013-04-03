15a99381c13f7dc543bc7dac92d2ffdca7648301
Walter Dastru <walter.dastru@gmail.com>
2013-04-03 17:15:42 +0200

    FantaCalcGui v3.3.5

--- website/private/fantacalcio/download.php
+++ website/private/fantacalcio/download.php
@@ -45,13 +45,15 @@ $numeriRomani = array( "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX
 		<br />
 
 		<table id='downloads' style="margin: auto; padding: 10px;">
-			<tr><td><img class='new' src='figure/new.gif'></td><td class='left'><a href="download/FantaCalcGui_v3.3.4_Setup.exe">FantaCalcGui v3.3.4 (installer)</a></td></tr>
-            <tr><td><img class='new' src='figure/new.gif'></td><td class='left'><a href="download/FantaCalcGui_v3.3.4.zip">FantaCalcGui v3.3.4.zip</a></td></tr>
-			<tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.3_Setup.exe">FantaCalcGui v3.3.3 (installer)</a></td></tr>
+	    <tr><td><img class='new' src='figure/new.gif'></td><td class='left'><a href="download/FantaCalcGui_v3.3.5_Setup.exe">FantaCalcGui v3.3.5 (installer)</a></td></tr>
+            <tr><td><img class='new' src='figure/new.gif'></td><td class='left'><a href="download/FantaCalcGui_v3.3.5.zip">FantaCalcGui v3.3.5.zip</a></td></tr>
+	    <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.4_Setup.exe">FantaCalcGui v3.3.4 (installer)</a></td></tr>
+            <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.4.zip">FantaCalcGui v3.3.4.zip</a></td></tr>
+	    <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.3_Setup.exe">FantaCalcGui v3.3.3 (installer)</a></td></tr>
             <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.3.zip">FantaCalcGui v3.3.3.zip</a></td></tr>
-			<tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.1_Setup.exe">FantaCalcGui v3.3.2 (installer)</a></td></tr>
+	    <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.1_Setup.exe">FantaCalcGui v3.3.2 (installer)</a></td></tr>
             <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.1.zip">FantaCalcGui v3.3.2.zip</a></td></tr>
-			<tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.1_Setup.exe">FantaCalcGui v3.3.1 (installer)</a></td></tr>
+	    <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.1_Setup.exe">FantaCalcGui v3.3.1 (installer)</a></td></tr>
             <tr><td></td><td class='left'><a href="download/FantaCalcGui_v3.3.1.zip">FantaCalcGui v3.3.1.zip</a></td></tr>
         </table>
 
