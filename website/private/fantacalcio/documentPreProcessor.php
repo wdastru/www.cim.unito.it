e4cfaa4b0e806549fb51d162d8254a675c5a9d67
Walter Dastrù <walter.dastru@gmail.com>
2013-07-16 08:10:09 +0200

    removed explicit colors in stylesheet.css

--- website/private/fantacalcio/documentPreProcessor.php
+++ website/private/fantacalcio/documentPreProcessor.php
@@ -51,6 +51,9 @@ if(file_exists($filename))
 	$content = str_replace('color6', $grigio_medio, $content);
 	$content = str_replace('color7', $grigio_scuro, $content);
 	$content = str_replace('color8', $grigio_scuro, $content);
+	$content = str_replace('htmlBkgdColor', $nero, $content);
+	$content = str_replace('bodyBkgdColor', $bianco, $content);
+	$content = str_replace('sorteggioColor', $rosso, $content);
 	/* * * * * * * * */
 	
 	/*
