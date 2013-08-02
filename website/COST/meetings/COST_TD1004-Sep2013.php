c4b398c13cb0dc710d5a6de6d3df02556bb5e7b7
Walter Dastru <walter.dastru@gmail.com>
2013-08-02 17:54:44 +0200

    da finire

---
+++ website/COST/meetings/COST_TD1004-Sep2013.php
@@ -0,0 +1,153 @@
+<?php $localizer = "../../"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <!-- InstanceBegin template="/Templates/events.dwt" codeOutsideHTMLIsLocked="false" -->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=cp1252" />
+        <meta name="generator" content=
+        "HTML Tidy for Windows (vers 14 February 2006), see www.w3.org" />
+        <!-- InstanceBeginEditable name="title" -->
+        <title>Molecular Imaging Center - University of Torino -
+            Meetings</title>
+        <!-- InstanceEndEditable -->
+        <!-- InstanceBeginEditable name="meta description" -->
+        <meta name="description" content="" />
+        <!-- InstanceEndEditable -->
+        <meta name="google-site-verification" content=
+        "q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0" />
+        <meta name="keywords" content=
+        "NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
+        <link href="<?php echo $localizer; ?>stylesheet.css" rel='stylesheet'
+        type='text/css' />
+        <link href="<?php echo $localizer; ?>COST/cost.css" rel='stylesheet' type='text/css' />
+        <script type="text/javascript" src="<?php echo $localizer; ?>script.js"></script>
+    </head>
+    <body>
+        <div id='section5'>
+            <?php
+            require ($localizer . 'includes/main-nav.php');
+            ?>
+            <div id='header'></div>
+            <!-- InstanceBeginEditable name="subsection opening" -->
+            <div id='subsection2'>
+                <!-- InstanceEndEditable -->
+                <div id='sidebar'>
+                    <?php
+                    require $localizer . 'COST/include/COST-sidebar.inc.php';
+                    ?>
+                </div>
+                <div id='content'>
+                    <div class='paddingOuter'>
+                        <div id='band'>
+                            <h1 id='subsectionTitle'><!-- InstanceBeginEditable name="subsection title" -->COST Action TD1004 <!-- InstanceEndEditable --></h1>
+                        </div>
+                        <div class='paddingInner'>
+                            <div style="padding: 0 27%">
+                                <a href="http://www.cost.esf.org/domains_actions/cmst/Actions/TD1004" target="_blank"> <img src="<?php echo $localizer ?>images/logo-COST.bmp" /></a>
+                            </div>
+                            <div style="clear:both">
+                                <br />
+                                <h1 style="text-align:center">COST Action TD1004</h1>
+                                <h1 style="text-align:center; line-height: 30px;">Annual Meeting</h1>
+                                <h2 style="text-align:center">Athens - Sep 1-3, 2013
+                                <br />
+                                </h2>
+                                <br />
+                                <div id='scientific_program' style="line-height: 30px;">
+
+                                    <a href="../../documenti/COST_TD_1004_Torino/Program_Athens_Annual Meeting_2013.pdf"><b>Download Programme</b></a>
+
+                                    <br />
+                                    <br />
+                                    <p style="font-weight:bold; font-size:14px">
+                                        SCIENTIFIC PROGRAM
+                                    </p>
+                                    <ul>
+                                        <li><u>SUNDAY, Sep 1<sup>st</sup>, 2013</u></li>
+                                        <ul>
+                                            <li>14:00 – 15:00 - Registration </li> 
+                                            <li>15:00 – 15:15 - Overall Lecture WG3<br/>
+                                                Robert Muller (University of Mons, Belgium): <i>Preparation and selection of targeting vectors</i></li>
+                                            <li>15:15 – 16:00 - Lectures WG3 :</li>
+                                            <ul>
+                                                <li>15:15 – 15:30 - <i>Development of High-Relaxivity and Luminescent Silica Nanoparticles As Multimodal Agents for Medical Imaging</i>, E. Lipani, S. Laurent, M. Surin, L. Vander Elst, P. Leclère, R.N. Muller</li>
+                                                <li>15:30 – 15:45 - <i>Theranostic nanoparticles for lung tumors</i>, F. Lux, A. Bianchi, S. Dufort, N. Tassali, J.-L. Coll, Y. Crémillieux, O. Tillement</li>
+                                                <li>15:45 – 16:00 - <i>Coordination polymer nanoparticles in medicine</i>, F. Nador, F. Novio, J. Lorenzo, D. Ruiz-Molina</li>
+                                            </ul>
+                                            <li>16:00 – 16:30 - Coffee Break</li>
+                                            <li>16:30 – 17:30 - Lectures WG3 (continued) :</li>
+                                                <ul>
+                                                    <li>16:30 – 16:45 - <i>Apoferritin loaded with Gd-HPDO3A and curcumin as a theranostic agent</i>, S. Geninatti</li>
+                                                    <li>16:45 – 17:00 - <i>Initial in vitro and in vivo assessment of Au@DTDTPA-RGD nanoparticles labeled with Ga-68</i>, C. Tsoukalas, G. Laurent, T. Tsotakos, R. Bazzi, M. Paravatou-Petsotas, S. Xanthopoulos, S. Roux and P. Bouziotis</li>
+                                                    <li>17:00 – 17:15 - <i>In vivo evaluation of functionalized polymeric NPs on memory deficit of Alzheimer’s disease mice model</i>, D. Carrodari, B. Le Droumaguet, J. Nicolas, D. Brambilla, E. Nespoli, E. Salvati, L. De Kimpe, C. Zona, C. Airoldi, C. Balducci, M. Zotti, M. Canovi, M. Gobbi, O. Flores, F. Nicotra, W. Scheper, F. Re, M. Masserini, G. Forloni, M. Salmona, P. Couvreur, K. Andrieux</li>
+                                                    <li>17:15 – 17:30 - <i>GN8 Fluorescent Analogues with Concomitant Therapeutic and Diagnostic Activities in Prion Diseases</i>, M. Staderini, S. Aulic, V. González-Ruiz, G. Bianchini, L. Morelli, A. D’Onofrio, H. Ngoc Ai Tran, N. Cabezas, M.A. Martín, G. Legname, J.C. Menéndez, M.L. Bolognesi</li>
+                                                </ul>
+                                            <li>18:00 - Museum Visit</li>
+                                            <li>20:30 - Dinner</li>
+                                        </ul>
+                                        <li><u>MONDAY, Sep 2<sup>nd</sup>, 2013</u></li>
+                                        
+                                        
+                                        
+                                        
+                                        <ul>
+                                            <li>9:00-10:30 - <i>Poster Session 1 | Image Guided Drug Delivery (Poster walk 05, Underground level II).</i> Annex 4, page 7</li>
+                                            <li>16:30 - 17:30 - <i>Plenary Lecture 3 | Mark Dewhirst - Harnessing imaging to achieve controlled drug delivery to tumors</i>
+                                                <br />
+                                                Chair: Gert Storm (Utrecht), Ivan Lukes (Prague) </li>
+                                            <li>18:00-19:00 - <i>Study Group Meeting | Image Guided Drug Delivery.</i> Annex 5, page 9
+                                                <br />
+                                                Chair: Chrit Moonen (Utrecht), Twan Lammers (Aachen) </li>
+                                            <li><i>Further oral and poster communications.</i> Annex 3, page 4</li>
+                                        </ul>
+                                        <li><u>TUESDAY, May 28, 2013</u></li>
+                                        <ul>
+                                            <li>13:00-14:30 - <i>Parallel Session 15 | Image Guided Drug Delivery.</i> Annex 6, page 10
+                                                <br />
+                                                Chair: Chrit Moonen (Utrecht), Twan Lammers (Aachen)</li>
+                                            <li><i>Further poster communications.</i> Annex 3, page 4</li>
+                                        </ul>
+                                    </ul>
+                                    <br />
+                                </div>
+                            </div>
+                            <!-- InstanceEndEditable -->
+                        </div>
+                    </div>
+                </div>
+                <!-- subsection closing -->
+            </div>
+            <!-- subsection closing -->
+            <div id='after'></div>
+            <div id='footer'>
+                <p>
+                    Molecular Imaging Center - Via Nizza, 52 - I-10125 Torino -
+                    ITALY
+                    <br />
+                    Fax. Tel. Mail
+                </p>
+            </div>
+            <div id='validators'>
+                <p>
+                    <a href="http://validator.w3.org/check?uri=referer"><img src=
+                    "images/valid-xhtml10-blue_opt.png" alt=
+                    "Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+                </p>
+                <p>
+                    <a href=
+                    "http://jigsaw.w3.org/css-validator/check?uri=referer"><img style=
+                    "border:0;width:88px;height:31px" src="images/vcss-blue_opt.gif"
+                    alt="CSS Valido!" /></a>
+                </p>
+            </div>
+            <!-- section closing -->
+        </div>
+        <!-- section closing -->
+        <script type="text/javascript">
+            //<![CDATA[
+            changeSideNavStyles();
+            //]]>
+        </script><!-- InstanceEnd -->
+    </body>
+</html>
