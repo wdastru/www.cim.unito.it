d2d8b9c08df3b7ef88aa3aba280c20f3522fd816
Walter Dastrù <walter.dastru@gmail.com>
2013-09-30 16:27:08 +0200

    added phpinfo.php

--- website/phpinfo.php
+++ website/phpinfo.php
@@ -1,14 +1,3 @@
 <?php
-        phpinfo();
+	phpinfo(); 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
-<html>
-<head>
-<meta name="generator" content=
-"HTML Tidy for Windows (vers 14 February 2006), see www.w3.org">
-<title></title>
-</head>
-<body>
-<?php require_once($localizer . 'includes/analyticstracking.php') ?>
-</body>
-</html>
