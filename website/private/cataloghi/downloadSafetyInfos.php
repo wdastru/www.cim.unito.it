9c615b7c4ab4ea63fa02a838a87413d286fc0be6
Walter Dastru <walter.dastru@gmail.com>
2013-11-08 12:17:03 +0100

    mod to downloadSafetyInfos.php

--- website/private/cataloghi/downloadSafetyInfos.php
+++ website/private/cataloghi/downloadSafetyInfos.php
@@ -1,7 +1,7 @@
 <?php
     
     $file = 'safety_info.tar.gz';
-    exec("tar -zcvf $file safety_info/");
+    exec("tar -zcf $file safety_info/");
     
     if (file_exists($file)) {
         header('Content-Description: File Transfer');
@@ -19,30 +19,4 @@
         exit;
     }
 
-    
-    //header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
-    //header("Content-type:   application/x-msexcel; charset=utf-8");
-    //header("Content-Disposition: attachment; filename=".$file_name.".xls"); 
-    //header("Expires: 0");
-    //header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-    //header("Cache-Control: private",false);
-    //print "$header\n$data";
-    //exit;
-   
-    
-    /*
-    $zipFile = "./safety_info/safety_info.zip";
-    $zipArchive = new ZipArchive();
-
-    if (!$zipArchive->open($zipFile, ZIPARCHIVE::OVERWRITE))
-        die("Failed to create archive\n");
-
-    $zipArchive->addGlob("./safety_info/*.*");
-    if (!$zipArchive->status == ZIPARCHIVE::ER_OK)
-        echo "Failed to write files to zip\n";
-
-    $zipArchive->close();
-    exit;
-     * 
-     */
 ?>
