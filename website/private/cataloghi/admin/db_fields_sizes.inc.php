fcff1ffc06c2eda17b153b62eb035b5f7e878fdd
Walter Dastru <walter.dastru@gmail.com>
2013-11-11 14:12:50 +0100

    modificato maxlength da 200 e 500

--- website/private/cataloghi/admin/db_fields_sizes.inc.php
+++ website/private/cataloghi/admin/db_fields_sizes.inc.php
@@ -1,11 +1,12 @@
 <?php
-    $name_UK_maxlength = '200';
-    $name_IT_maxlength = '200';
+    $name_UK_maxlength = '500';
+    $name_IT_maxlength = '500';
     $place_maxlength = '10';
     $quantity_maxlength = '2';
     $lab_maxlength = '10';
     $note_maxlength = '100';
     $CAS_maxlength = '20';
     $phrase_H_maxlength = '50';
-    $phrase_R_maxlength = '50'; 
+    $phrase_R_maxlength = '50';
+    $link_maxlength = '500';
 ?>
