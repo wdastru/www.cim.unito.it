2c68d647784f1a5612d0b6f3c419d0f5f1c15371
Walter Dastru <walter.dastru@gmail.com>
2013-11-15 16:05:52 +0100

    prova 2

--- website/private/cataloghi/index.php
+++ website/private/cataloghi/index.php
@@ -37,6 +37,7 @@ session_start();
 foreach ($_POST as $key => $value) 
 {
     $_POST[$key] = str_replace("\'", "'", $value);
+    $_POST[$key] = htmlentities($_POST[''], ENT_QUOTES);
 }
 
 $_FILES['newlink']['name'] = str_replace("\'", "", $_FILES['newlink']['name']);
@@ -76,7 +77,7 @@ if ($_SESSION['nfields'] == "") {
 	$_SESSION['nfields'] = 1;
 } else {
 	$_SESSION['nfields'] = $_SESSION['nfields'] + $_POST['add'] - $_POST['delete'];
-	if ($_SESSION['nfields'] == 0) {	$_SESSION['nfields'] = 1;
+	   if ($_SESSION['nfields'] == 0) {	$_SESSION['nfields'] = 1;
 	}
 }
 
@@ -228,28 +229,28 @@ $_POST['delete'] = 0;
                                     if ($_FILES['newlink']['name'] != "") {
                                         
                                         $sql = "UPDATE catalogo
-                				SET 	
-                					name_UK='"  . mysql_real_escape_string($_POST['newname_UK'])       . "',
-                                                    	name_IT='"  . mysql_real_escape_string($_POST['newname_IT'])       . "',
-                                                    	place='"    . mysql_real_escape_string($_POST['newplace'])         . "',
-                					quantity='" . mysql_real_escape_string($_POST['newquantity'])      . "',
-                					lab='"      . mysql_real_escape_string($_POST['newlab'])           . "',
-                					note='"     . mysql_real_escape_string($_POST['newnote'])          . "',
-                                                    	CAS='"      . mysql_real_escape_string($_POST['newCAS'])           . "',
-                                                   	phrase_H='" . mysql_real_escape_string($_POST['newphrase_H'])      . "',
-                                                    	phrase_R='" . mysql_real_escape_string($_POST['newphrase_R'])      . "',
-                					link='"     . mysql_real_escape_string($_FILES['newlink']['name']) . "'
-                				WHERE 	
-                					name_UK='"  . mysql_real_escape_string($_POST['oldname_UK'])    . "' AND
-                                                    	name_IT='"  . mysql_real_escape_string($_POST['oldname_IT'])    . "' AND
-                                                    	place='"    . mysql_real_escape_string($_POST['oldplace'])      . "' AND
-                					quantity='" . mysql_real_escape_string($_POST['oldquantity'])   . "' AND
-                					lab='"      . mysql_real_escape_string($_POST['oldlab'])        . "' AND
-                					note='"     . mysql_real_escape_string($_POST['oldnote'])       . "' AND
-                                                    	CAS='"      . mysql_real_escape_string($_POST['oldCAS'])        . "' AND
-                                                    	phrase_H='" . mysql_real_escape_string($_POST['oldphrase_H'])   . "' AND									
-                					phrase_R='" . mysql_real_escape_string($_POST['oldphrase_R'])   . "' AND
-                					link='"     . mysql_real_escape_string($_POST['oldlink'])       . "'";
+                                				SET 	
+                                					name_UK='"  . mysql_real_escape_string($_POST['newname_UK'])       . "',
+                                                    name_IT='"  . mysql_real_escape_string($_POST['newname_IT'])       . "',
+                                                    place='"    . mysql_real_escape_string($_POST['newplace'])         . "',
+                                					quantity='" . mysql_real_escape_string($_POST['newquantity'])      . "',
+                                					lab='"      . mysql_real_escape_string($_POST['newlab'])           . "',
+                                					note='"     . mysql_real_escape_string($_POST['newnote'])          . "',
+                                                    CAS='"      . mysql_real_escape_string($_POST['newCAS'])           . "',
+                                                    phrase_H='" . mysql_real_escape_string($_POST['newphrase_H'])      . "',
+                                                    phrase_R='" . mysql_real_escape_string($_POST['newphrase_R'])      . "',
+                                					link='"     . mysql_real_escape_string($_FILES['newlink']['name']) . "'
+                                				WHERE 	
+                                					name_UK='"  . mysql_real_escape_string($_POST['oldname_UK'])    . "' AND
+                                                    name_IT='"  . mysql_real_escape_string($_POST['oldname_IT'])    . "' AND
+                                                    place='"    . mysql_real_escape_string($_POST['oldplace'])      . "' AND
+                                					quantity='" . mysql_real_escape_string($_POST['oldquantity'])   . "' AND
+                                					lab='"      . mysql_real_escape_string($_POST['oldlab'])        . "' AND
+                                					note='"     . mysql_real_escape_string($_POST['oldnote'])       . "' AND
+                                                    CAS='"      . mysql_real_escape_string($_POST['oldCAS'])        . "' AND
+                                                    phrase_H='" . mysql_real_escape_string($_POST['oldphrase_H'])   . "' AND									
+                                					phrase_R='" . mysql_real_escape_string($_POST['oldphrase_R'])   . "' AND
+                                					link='"     . mysql_real_escape_string($_POST['oldlink'])       . "'";
         							} else {
         							    
                                         $sql = "UPDATE catalogo
@@ -264,7 +265,7 @@ $_POST['delete'] = 0;
                                                     	phrase_H='" . mysql_real_escape_string($_POST['newphrase_H'])    . "',
                                                     	phrase_R='" . mysql_real_escape_string($_POST['newphrase_R'])    . "' 
                                                 WHERE   
-                                                    	name_UK='"  . mysql_real_escape_string(htmlentities($_POST['oldname_UK'], ENT_QUOTES))  . "'"/* AND
+                                                    	name_UK='"  . mysql_real_escape_string($_POST['oldname_UK'])  . "' AND
                                                     	name_IT='"  . mysql_real_escape_string($_POST['oldname_IT'])  . "' AND
                                                     	place='"    . mysql_real_escape_string($_POST['oldplace'])    . "' AND
                                                     	quantity='" . mysql_real_escape_string($_POST['oldquantity']) . "' AND
