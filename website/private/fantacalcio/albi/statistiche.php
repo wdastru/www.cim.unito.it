f1f9d689d81928a4aef6c4a4d5041ff045562eb2
Walter Dastrù <walter.dastru@gmail.com>
2014-01-01 17:48:34 +0100

    lettura di allTimeStats.txt iniziata

--- website/private/fantacalcio/albi/statistiche.php
+++ website/private/fantacalcio/albi/statistiche.php
@@ -20,23 +20,6 @@ $relocate_string = "../";
 <!-- <body onresize="Count();"> -->
 <body>
 
-<?php	
-
-//$files[0] = $relocate_string . 'website/private/fantacalcio/777/datiCampionato.txt';
-//$files[1] = $relocate_string . 'website/private/fantacalcio/archivio/2008/datiCampionato0708.txt';
-//$files[2] = $relocate_string . 'website/private/fantacalcio/archivio/2009/datiCampionato0809.txt';
-//$files[3] = $relocate_string . 'website/private/fantacalcio/archivio/2010/datiCampionato0910.txt';
-//$files[4] = $relocate_string . 'website/private/fantacalcio/archivio/2011/datiCampionato1011.txt';
-//$files[5] = $relocate_string . 'website/private/fantacalcio/archivio/2012/datiCampionato1112.txt';
-//$files[6] = $relocate_string . 'website/private/fantacalcio/archivio/2013/datiCampionato1213.txt';
-//
-//foreach ($files as $file) {
-//	system("cat '$file'");
-//	echo '<br />';
-//}
-
-?>
-
 <?php require $relocate_string . 'include/title.inc.php'; ?>
 <?php require $relocate_string . 'include/menu.inc.php'; ?>
 	<!-- InstanceBeginEditable name="hiddenBox" -->
@@ -45,6 +28,21 @@ $relocate_string = "../";
 		<!-- InstanceBeginEditable name="body" -->
 		<h1 class="title">STATISTICHE</h1>
 		<br />
+		
+		<?php 
+			$handle = fopen($relocate_string . '777/allTimeStats.txt', 'r');
+			$i = 0;
+			while (!feof($handle)) {
+				$stats[$i] = fgets($handle); 
+				$i++;
+			}
+			fclose($handle);
+			
+			foreach($stats as $stat) {
+				echo "<pre>$stat</pre>";
+			}
+		?>
+		
 	</div>
 	<?php include $relocate_string . 'include/footer.inc.php'?>
 	
