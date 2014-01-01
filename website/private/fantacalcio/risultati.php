7c53b179c86cd22b9bf3d6174e50ddf20a32696d
Walter Dastrù <walter.dastru@gmail.com>
2014-01-01 16:55:44 +0100

    creazione di allTimeStats.txt in 777/ quasi ok

--- website/private/fantacalcio/risultati.php
+++ website/private/fantacalcio/risultati.php
@@ -88,22 +88,33 @@ include("calendario.inc");
 		
 		<?php
 		
-		$files[6] = $relocate_string . '777/datiCampionato.txt';
-		$files[5] = $relocate_string . 'archivio/2013/datiCampionato1213.txt';
-		$files[4] = $relocate_string . 'archivio/2012/datiCampionato1112.txt';
-		$files[3] = $relocate_string . 'archivio/2011/datiCampionato1011.txt';
-		$files[2] = $relocate_string . 'archivio/2010/datiCampionato0910.txt';
-		$files[1] = $relocate_string . 'archivio/2009/datiCampionato0809.txt';
-		$files[0] = $relocate_string . 'archivio/2008/datiCampionato0708.txt';
-		$year[6] = '1314';
-		$year[5] = '1213';
-		$year[4] = '1112';
-		$year[3] = '1011';
-		$year[2] = '0910';
-		$year[1] = '0809';
-		$year[0] = '0708';
-				
-		$allTimeStatsHandle = fopen("allTimeStats.txt", 'w');
+		$files[0] = $relocate_string . '777/datiCampionato.txt';
+		/*
+		//$files[5] = $relocate_string . 'archivio/2013/datiCampionato1213.txt';
+		//$files[4] = $relocate_string . 'archivio/2012/datiCampionato1112.txt';
+		//$files[3] = $relocate_string . 'archivio/2011/datiCampionato1011.txt';
+		//$files[2] = $relocate_string . 'archivio/2010/datiCampionato0910.txt';
+		//$files[1] = $relocate_string . 'archivio/2009/datiCampionato0809.txt';
+		//$files[0] = $relocate_string . 'archivio/2008/datiCampionato0708.txt';
+		 */
+		$year[0] = '1314';
+		/*
+		//$year[5] = '1213';
+		//$year[4] = '1112';
+		//$year[3] = '1011';
+		//$year[2] = '0910';
+		//$year[1] = '0809';
+		//$year[0] = '0708';
+		 */
+		
+		if (copy($relocate_string . "allTimeStatsOld.txt", $relocate_string . "777/allTimeStats.txt")) {
+			$allTimeStatsHandle = fopen($relocate_string . "777/allTimeStats.txt", 'a');
+		} else {
+			/* throw exception
+			 * if file copy
+			 * fails
+			 */
+		}
 		
 for($filesCounter=0; $filesCounter<count($files); $filesCounter++) {
 	
