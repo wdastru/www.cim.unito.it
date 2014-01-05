34e4a1232c785442291bb94c34ecc44f89df68b5
Walter Dastrù <walter.dastru@gmail.com>
2014-01-05 08:17:52 +0100

    riordinato il codice

--- website/private/fantacalcio/risultati.php
+++ website/private/fantacalcio/risultati.php
@@ -172,35 +172,32 @@ include("calendario.inc");
 			echo "</div>\n";
 		}
 		
-		$files[0] = $relocate_string . '777/datiCampionato.txt';
-		/*
-		 * usati per creare i dati statistici storici 
-		 * di allTimeStatsOld.txt
-		 * */
-		//$files[5] = $relocate_string . 'archivio/2013/datiCampionato1213.txt';
-		//$files[4] = $relocate_string . 'archivio/2012/datiCampionato1112.txt';
-		//$files[3] = $relocate_string . 'archivio/2011/datiCampionato1011.txt';
-		//$files[2] = $relocate_string . 'archivio/2010/datiCampionato0910.txt';
-		//$files[1] = $relocate_string . 'archivio/2009/datiCampionato0809.txt';
-		//$files[0] = $relocate_string . 'archivio/2008/datiCampionato0708.txt';
-		 /**/
-		$year[0] = '1314';
-		/**/
-		//$year[5] = '1213';
-		//$year[4] = '1112';
-		//$year[3] = '1011';
-		//$year[2] = '0910';
-		//$year[1] = '0809';
-		//$year[0] = '0708';
-		 /**/
-		
-		if (copy($relocate_string . "allTimeStatsOld.txt", $relocate_string . "777/allTimeStats.txt")) {
-			$allTimeStatsHandle = fopen($relocate_string . "777/allTimeStats.txt", 'a');
+		$createOldStats = 0;
+		if ($createOldStats) {
+			$files[5] = $relocate_string . 'archivio/2013/datiCampionato1213.txt';
+			$files[4] = $relocate_string . 'archivio/2012/datiCampionato1112.txt';
+			$files[3] = $relocate_string . 'archivio/2011/datiCampionato1011.txt';
+			$files[2] = $relocate_string . 'archivio/2010/datiCampionato0910.txt';
+			$files[1] = $relocate_string . 'archivio/2009/datiCampionato0809.txt';
+			$files[0] = $relocate_string . 'archivio/2008/datiCampionato0708.txt';
+			$year[5] = '1213';
+			$year[4] = '1112';
+			$year[3] = '1011';
+			$year[2] = '0910';
+			$year[1] = '0809';
+			$year[0] = '0708';
+			$allTimeStatsHandle = fopen($relocate_string . "allTimeStatsOld.txt", 'w');
 		} else {
-			/* throw exception
-			 * if file copy
-			 * fails
-			 */
+			$files[0] = $relocate_string . '777/datiCampionato.txt';
+			$year[0] = '1314';
+			if (copy($relocate_string . "allTimeStatsOld.txt", $relocate_string . "777/allTimeStats.txt")) {
+				$allTimeStatsHandle = fopen($relocate_string . "777/allTimeStats.txt", 'a');
+			} else {
+				/* throw exception
+				 * if file copy
+				 * fails
+				 */
+			}
 		}
 		
 		for($filesCounter=0; $filesCounter<count($files); $filesCounter++) {
