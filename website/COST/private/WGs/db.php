7386dc3508c136681127bf8a0ccb0dd04a769c46
Walter Dastru <walter.dastru@gmail.com>
2014-02-04 11:25:49 +0100

    problema upload file COST 7

--- website/COST/private/WGs/db.php
+++ website/COST/private/WGs/db.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-print_r($_POST);
-echo "<br />";
-print_r($_FILES);
+//print_r($_POST);
+//echo "<br />";
+//print_r($_FILES);
 
 /* script variables */
 $localizer = "../../../";
@@ -35,13 +35,13 @@ if (isset($_POST['WG'])) {
 	}
 }
 
-echo "<br>WG : " . $WG;
+//echo "<br>WG : " . $WG;
 
 if (isset($_POST['desc'])) {
 	$desc = htmlentities($_POST['desc']);
 }
 
-echo "<br>desc : " . $desc;
+//echo "<br>desc : " . $desc;
 
 if (isset($_POST['type']) && $_POST['type']=='add') {
 	if (isset($_FILES['filename']['name'])) {
@@ -52,7 +52,7 @@ if (isset($_POST['type']) && $_POST['type']=='add') {
 		header("Location: " . $localizer . "COST/private/error.php?error=missing_upload_file");
 		exit();
 	}
-	echo "<br>addFilename : " . $addFilename;
+//	echo "<br>addFilename : " . $addFilename;
 } else if (isset($_POST['type']) && $_POST['type']=='del') {
     if (isset($_POST['filename'])) {
         $delFilename = $_POST['filename'];
@@ -60,20 +60,20 @@ if (isset($_POST['type']) && $_POST['type']=='add') {
         header("Location: " . $localizer . "COST/private/error.php?error=missing_filename");
         exit();
     }
-    echo "<br>delFilename : " . $delFilename;
+//    echo "<br>delFilename : " . $delFilename;
 }
 
 if (isset($_POST['date'])) {
     $date = htmlentities($_POST['date']);
 }
 
-echo "<br>date : " . $date;
+//echo "<br>date : " . $date;
 
 if (isset($_POST['path'])) {
     $path = htmlentities($_POST['path']);
 }
 
-echo "<br>path : " . $path;
+//echo "<br>path : " . $path;
 
 if (isset($_POST['type'])) {
 	if (preg_match('/^(add|del)$/', $_POST['type']))
@@ -82,7 +82,7 @@ if (isset($_POST['type'])) {
 	}
 }
 
-echo "<br>type : " . $type;
+//echo "<br>type : " . $type;
 
 if (isset($_POST['table'])) {
     $table = $_POST['table'];
@@ -93,7 +93,7 @@ else
 	exit();
 }
 
-echo "<br>table : " . $table;
+//echo "<br>table : " . $table;
 
 mysql_select_db($DBName, $con);
 
@@ -102,31 +102,31 @@ $sql = "OPTIMIZE TABLE `" . $DBName . "`.`" . $table . "`";
 mysql_query($sql, $con);
 /* */
 
-echo "<br />0";
+//echo "<br />0";
 
 if ($type == "add") 
 {
-echo "<br />1";
+//echo "<br />1";
     if (!file_exists($path . $addFilename)) 
     {
-echo "<br />2";
+//echo "<br />2";
         if (move_uploaded_file($_FILES['filename']['tmp_name'], $path . $addFilename)) 
 	{
-echo "<br />3";
+//echo "<br />3";
             $sql = "INSERT INTO `" . $DBName . "`.`" . $table . "` ( `WG`, `desc`, `filename`, `date`, `path` ) VALUES ( '" . $WG . "', '" . $desc . "', '" . $addFilename . "', '" . $date . "', '" . $path . "')";
 		$result = mysql_query($sql, $con);
 
 	} 
 	else 
 	{
-echo "<br />8";
+//echo "<br />8";
 		header("Location: " . $localizer . "COST/private/error.php?error=upload");
 		exit();
         }
     } 
     else 
     {
-echo "<br />9";
+//echo "<br />9";
         header("Location: " . $localizer . "COST/private/error.php?error=file_exists");
         exit();
     }
@@ -134,31 +134,33 @@ echo "<br />9";
 } 
 else if ($type == "del") 
 {
-echo "<br />4";
+//echo "<br />4";
 	$sql = "DELETE FROM `" . $DBName . "`.`" . $table . "` WHERE `date` = '" . $date . "' AND `filename` = '" . $delFilename . "' AND `WG` = '" . $WG . "' AND `path` = '" . $path . "' AND `desc` = '" . $desc . "'";
 	$result = mysql_query($sql, $con);
 	
 	if (file_exists($path . $delFilename)) {
-echo "<br />5";
+//echo "<br />5";
 		if (!unlink($path . $delFilename)) {
-echo "<br />10 : $path";
-echo "<br />10 : $delFileName";
+//echo "<br />10 : $path";
+//echo "<br />10 : $delFileName";
 			header("Location: " . $localizer . "COST/private/error.php?error=could_not_delete_file");
 			exit();
 	    }
 	} else {
-echo "<br />6";
+//echo "<br />6";
 		header("Location: " . $localizer . "COST/private/error.php?error=file_not_exists");
 		exit();
 	}
 
 } else {
-echo "<br />7";
+//echo "<br />7";
 	header("Location: " . $localizer . "COST/private/error.php?error=invalid_type");
 	exit();
 }
 
 mysql_close($con);
+
+//echo "<br />" . $localizer . "COST/private/WGs/WG.php?WG=" . $WG;
 
 header("Location: " . $localizer . "COST/private/WGs/WG.php?WG=" . $WG);
 ?>
