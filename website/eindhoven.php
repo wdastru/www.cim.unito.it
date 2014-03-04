c2d59cc0dcf6b48312942adea70799c33c9b4687
Walter Dastru <walter.dastru@gmail.com>
2014-03-04 14:25:40 +0100

    fixing validation errors 4

--- website/eindhoven.php
+++ website/eindhoven.php
@@ -68,15 +68,15 @@
 							</h2>
 							<p>
 								<img class='images' src=
-								'images/BvOF%202011%20TUe%20Dies%203.jpg' />
+								'images/BvOF%202011%20TUe%20Dies%203.jpg' alt='image' />
 							</p>
 							<p>
 								<img class='images' src=
-								'images/BvOF%202011%20TUe%20Dies%206.jpg' />
+								'images/BvOF%202011%20TUe%20Dies%206.jpg' alt='image' />
 							</p>
 							<p>
 								<img class='images' src=
-								'images/BvOF%202011%20TUe%20Dies%207.jpg' />
+								'images/BvOF%202011%20TUe%20Dies%207.jpg' alt='image' />
 							</p>
 							<!-- InstanceEndEditable -->
 						</div>
