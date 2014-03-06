27f9a3bef5a67d244de9fe576eb8228a190a4da4
Walter Dastru <walter.dastru@gmail.com>
2014-03-06 18:33:42 +0100

    fixing validation errors 2

--- website/CEST2014/submission.php
+++ website/CEST2014/submission.php
@@ -132,7 +132,7 @@ xmlns="http://www.w3.org/1999/xhtml">
                                 
                                 <b>Submission rules</b>
                                 <br/>
-                                
+
                                 <ul>
                                     <li>size limit: 1 page (max 2 MB)</li>
                                     <li>only Word (DOC or DOCX) or PDF documents are allowed</li>
@@ -140,21 +140,23 @@ xmlns="http://www.w3.org/1999/xhtml">
                                     <li>see the <a href="documents/ABSTRACT_TEMPLATE.docx"><span class='underline'>abstract template</span></a></li>
                                     <li>you will be able to make changes on your submission until the submission deadline (1<sup>st</sup> March 2014)</li>
                                 </ul>
-                                
+
                                 <br/>
                                 <br/>
-                                
+
                                 <b>Online submission</b>
-                            
+
                             	<form action='upload.php' method='post' enctype='multipart/form-data' onsubmit='return checkSizeAndExtension();'>
-                                    <span>Submitter&nbsp;:&nbsp;</span><input class='required' type='text' name='submitter' value='' onkeyup='return checkSubmitterField();'/>
-                                    <span style='color: #db6d16'>&nbsp;&nbsp;required field</span><br/><br/>
+                                    <!--<span>Submitter&nbsp;:&nbsp;</span>-->
+                                    <input class='required' type='text' name='submitter' value='' onkeyup='return checkSubmitterField();'/>
+                                    <!--<span style='color: #db6d16'>&nbsp;&nbsp;required field</span><br/><br/>-->
                                     <input type='file' name='file1' id='file1' style='width:100%'/>
                                     <input type='hidden' name='dir' value='<?php echo $localizer; ?>/uploads/' style='width:100%'/>
                                     <br /><br /> 
                                     <input type='hidden' name='submitted' value='1' />
                                     <input id='submissionButton' type='submit' name='submit' value='Submit' disabled='true' />
                                 </form>
+                                
                             </div>
                         </div>
                     </div>
