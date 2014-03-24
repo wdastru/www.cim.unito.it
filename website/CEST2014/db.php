3544cbbd26257aa6f757cba3db8fcbf69342c8a9
Walter Dastru <walter.dastru@gmail.com>
2014-03-24 17:21:30 +0100

    mod attendance.php in CEST2014

--- website/CEST2014/db.php
+++ website/CEST2014/db.php
@@ -1,35 +1,34 @@
 <?php
-session_start();
-
-//print_r($_POST);
-
-$con = mysql_connect("localhost", "cim_adm","vpsyyAR4jp");
-if (!$con)
-{
-    die('Could not connect: ' . mysql_error());
+    session_start();
+    
+    //print_r($_POST);
+    
+    $con = mysql_connect("localhost", "cim_adm","vpsyyAR4jp");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+        exit();
+    }
+    
+    $DBName = "cimdb";
+    $table = "arrivals";
+    
+    mysql_select_db($DBName, $con);
+    
+    $sql = "INSERT INTO `" . $DBName . "`.`" . $table . "` ( `surname` , `name` , `email` , `airport` , `hour` , `leave` , `accompanying` ) VALUES ( '" . $_POST['surname'] . " ', '" . $_POST['name'] . "', '" . $_POST['email'] . "', '" . $_POST['airport'] . "', '" . $_POST['hour'] . "', '" . $_POST['leave'] . "', '" . $_POST['accompanying'] . "')";
+    
+    //echo "<br/>$sql";
+    
+    $query = mysql_query($sql, $con);
+    
+    if (!$query)
+    {
+        die('<br/>Query failed: ' . mysql_error());
+        exit();
+    }
+    
+    mysql_close($con);
+    
+    header("Location: attendance.php?confirm=yes");
     exit();
-}
-
-$DBName = "cimdb";
-$table = "arrivals";
-
-mysql_select_db($DBName, $con);
-
-$sql = "INSERT INTO `" . $DBName . "`.`" . $table . "` ( `surname` , `name` , `email` , `airport` , `hour` , `leave` , `accompanying` ) VALUES ( '" . 
-$_POST['surname'] . " ', '" . $_POST['name'] . "', '" . $_POST['email'] . "', '" . $_POST['airport'] . "', '" . $_POST['hour'] . "', '" . $_POST['leave'] . "', '" . $_POST['accompanying'] . "')";
-
-//echo "<br/>$sql";
-
-$query = mysql_query($sql, $con);
-
-if (!$query)
-{
-    die('<br/>Query failed: ' . mysql_error());
-    exit();
-}
-
-mysql_close($con);
-
-header("Location: attendance.php?confirm=yes");
-exit();
 ?>
