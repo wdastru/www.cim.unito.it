2bd986bff03dfa201b9616f52dacadcd759fa6bc
Walter Dastru <walter.dastru@gmail.com>
2014-03-26 15:06:15 +0100

    added final program to CEST2014

--- website/CEST2014/about.php
+++ website/CEST2014/about.php
@@ -124,7 +124,7 @@ xmlns="http://www.w3.org/1999/xhtml">
                                             Abstract submission : <span style="color: #db6d16">1<sup>st</sup> March 2014</span>
                                         </li>
                                         <li>
-                                            Definitive program : <span style="color: #db6d16">Soon available</span>
+                                            <a href="documents/CEST_2014_Workshop_Programme.pdf">Definitive program</a>
                                         </li>
                                         <li>
                                             Early registration : <span style="color: #db6d16">11 April 2014</span>
