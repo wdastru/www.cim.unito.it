190b716ba7054e676ffebfa61e3eb0ff62e0c3f3
Walter Dastrù <walter.dastru@gmail.com>
2015-02-26 19:07:35 +0100

    start

---
+++ website/includes/head_const.inc.php
@@ -0,0 +1,25 @@
+<?php
+echo "
+
+	<meta name='generator' content='HTML Tidy for Windows (vers 14 February 2006), see www.w3.org' />
+    <!-- InstanceBeginEditable name='title' -->
+    <title>Molecular Imaging Center - University of Torino</title>
+    <!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name='meta description' -->
+    <meta name='description' content='University of Torino. Website of the Molecular Imaging Center.' />
+    <!-- InstanceEndEditable -->
+    <meta name='google-site-verification' content='q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0' />
+    <meta http-equiv='Content-Type' content='text/php; charset=us-ascii' />
+    <meta name='keywords' content='NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia' />
+    <link href='stylesheet.css' rel='stylesheet' type='text/css'></link>
+    <link rel='icon' href='images/favicon.gif' type='image/gif'></link>
+    <script type='text/javascript' src='script.js'></script>
+    
+    ";
+ 
+        
+       /* InstanceBeginEditable name='jQuery' */
+       require($localizer . 'includes/jQuery.php');
+       /* InstanceEndEditable */
+        
+?>
--- website/index.php
+++ website/index.php
@@ -6,30 +6,7 @@ $localizer = "./";
 xmlns="http://www.w3.org/1999/xhtml">
     <!-- InstanceBegin template="/Templates/home.dwt" codeOutsidephpIsLocked="false" -->
     <head>
-        <meta name="generator"
-        content="HTML Tidy for Windows (vers 14 February 2006), see www.w3.org" />
-        <!-- InstanceBeginEditable name="title" -->
-        <title>Molecular Imaging Center - University of Torino</title>
-        <!-- InstanceEndEditable -->
-        <!-- InstanceBeginEditable name="meta description" -->
-        <meta name="description"
-        content="University of Torino. Website of the Molecular Imaging Center." />
-        <!-- InstanceEndEditable -->
-        <meta name="google-site-verification"
-        content="q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0" />
-        <meta http-equiv="Content-Type" content="text/php; charset=us-ascii" />
-        <meta name="keywords"
-        content="NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
-        <link href="stylesheet.css" rel='stylesheet' type='text/css'></link>
-        <link rel="icon" href="images/favicon.gif" type="image/gif"></link>
-        <script type="text/javascript" src="script.js"></script>
-        
-        <!-- InstanceBeginEditable name="jQuery" -->
-        <?php
-            require($localizer . 'includes/jQuery.php');
-        ?>
-        <!-- InstanceEndEditable -->
-        
+	    <?php require $localizer . 'includes/head_const.inc.php'?>
     </head>
     <body>
         <?php require_once($localizer . 'includes/analyticstracking.php') ?>
