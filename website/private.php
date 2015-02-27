40963aee4b4d8801bba61383ec7c37f9866589df
Walter Dastrù <walter.dastru@gmail.com>
2015-02-27 10:21:01 +0100

    meta-keywords

--- website/private.php
+++ website/private.php
@@ -16,7 +16,7 @@ meta-gen
 
 meta-google
 <meta http-equiv="Content-Type" content="text/php; charset=us-ascii" />
-<meta name="keywords" content="NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
+meta-keywords
 <link href='<?php echo $localizer . "stylesheet.css"?>' rel='stylesheet' type='text/css'></link>
 <link rel="icon" href='<?php echo $localizer . "images/favicon.gif"?>' type="image/gif"></link>
 <script type="text/javascript" src='<?php echo $localizer . "script.js"?>'></script>
--- website/private/cataloghi/admin/add.php
+++ website/private/cataloghi/admin/add.php
@@ -23,7 +23,7 @@ xmlns="http://www.w3.org/1999/xhtml">
         
         meta-google
         <meta http-equiv="Content-Type" content="text/php; charset=us-ascii" />
-        <meta name="keywords" content="NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
+        meta-keywords
         <link rel="icon" href="images/favicon.gif" type="image/gif" />
         <link href="<?php echo $localizer; ?>stylesheet.css" rel='stylesheet' type='text/css' />
         <link href="<?php echo $localizer; ?>private/cataloghi/catalog.css" rel='stylesheet' type='text/css' />
