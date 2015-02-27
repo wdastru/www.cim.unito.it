d57ba9caf98109bcfa7140854304a77418163ea5
Walter Dastrù <walter.dastru@gmail.com>
2015-02-27 08:04:58 +0100

    meta-gen

--- website/COST/link_1.php
+++ website/COST/link_1.php
@@ -7,8 +7,7 @@ $localizer = "../";
 	xmlns="http://www.w3.org/1999/xhtml">
 <!-- InstanceBegin template="/Templates/home.dwt" codeOutsidephpIsLocked="false" -->
 <head>
-<meta name="generator"
-	content="HTML Tidy for Windows (vers 14 February 2006), see www.w3.org" />
+meta-gen
 <!-- InstanceBeginEditable name="title" -->
 <title>Molecular Imaging Center - University of Torino</title>
 <!-- InstanceEndEditable -->
