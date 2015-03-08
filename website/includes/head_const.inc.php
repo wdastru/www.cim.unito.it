5fdbc9fd1259046c00d350ac10861b60af6430a3
Walter Dastrù <walter.dastru@gmail.com>
2015-03-08 01:15:12 +0100

    fixed head_const.inc.php

--- website/includes/head_const.inc.php
+++ website/includes/head_const.inc.php
@@ -1,15 +1,15 @@
 <?php
 echo "
 
-	meta-gen
-    <title>Molecular Imaging Center - University of Torino</title>
+	<title>Molecular Imaging Center - University of Torino</title>
     <meta name='description' content='University of Torino. Website of the Molecular Imaging Center.' />
     <meta name='google-site-verification' content='q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0' />
-    meta-http
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta http-equiv='Content-Type' content='text/php; charset=us-ascii' />
     <meta name='keywords' content='NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia' />
-    <link href='<?php echo $localizer; ?>stylesheet.css' rel='stylesheet' type='text/css' />
-    <link href='<?php echo $localizer;?>images/favicon.gif' rel='icon' type='image/gif' />
-    <script type='text/javascript' src='script.js' />
+    <link href='" . $localizer . "stylesheet.css' rel='stylesheet' type='text/css' />
+    <link href='" . $localizer . "images/favicon.gif' rel='icon' type='image/gif' />
+    <script src='" . $localizer . "script.js' type='text/javascript' />
     
     ";
  
