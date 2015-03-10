2db5a9a8c796711fa146da9928bcb2b5ccce07e4
Walter Dastru <walter.dastru@gmail.com>
2015-03-10 17:59:01 +0100

    added Cache-control:public

--- website/includes/head_const.inc.php
+++ website/includes/head_const.inc.php
@@ -6,6 +6,7 @@ echo "
     <meta name='google-site-verification' content='q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0' />
     <meta name='viewport' content='width=device-width, initial-scale=1' />
     <meta http-equiv='Content-Type' content='text/php; charset=us-ascii' />
+    <meta http-equiv='Cache-control' content='public'>
     <meta name='keywords' content='NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia' />
     <link href='" . $localizer . "stylesheet.css' rel='stylesheet' type='text/css' />
     <link href='" . $localizer . "images/favicon.gif' rel='icon' type='image/gif' />
