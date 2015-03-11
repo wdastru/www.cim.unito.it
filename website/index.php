794fdc430a9790fde4a13a004e83c57d03e260d5
Walter Dastru <walter.dastru@gmail.com>
2015-03-11 15:21:26 +0100

    added php header(...) in index.php

--- website/index.php
+++ website/index.php
@@ -1,6 +1,7 @@
 <?php
 $localizer = "./";
-ob_start("ob_gzhandler");
+ob_start('ob_gzhandler');
+header('Content-Type: text/html;charset=UTF-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html
