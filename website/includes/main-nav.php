3414c875888c15b61dc3eda045e899e18d9362dd
Walter Dastru <walter.dastru@gmail.com>
2015-03-20 18:13:01 +0100

    dopo start

--- website/includes/main-nav.php
+++ website/includes/main-nav.php
@@ -1,6 +1,6 @@
 <?php
 	echo "
-		<!--
+		<!-- -->
 		<div id='main-nav'>
 			<dl>
 				<dt id='main-nav-section0'>
@@ -29,18 +29,22 @@
 				</dt>
 			</dl>
 		</div>
-		-->
-		
+	<!-- -->
+	<!--	
 	<nav id='menu'>
         <ul class='navigation'>
             <a  class='main active' href='#'>Navigation</a>
-            <li class='n1'><a href='#'>Item</a></li>
-            <li class='n2'><a href='#'>Item</a></li>
-            <li class='n3'><a href='#'>Item</a></li>
-            <li class='n4'><a href='#'>Item</a></li>
-            <li class='n5'><a href='#'>Item</a></li>
+            <li id='main-nav-section0' class='n1'><a href='".$localizer."index.php'>HOME</a></li>
+            <li id='main-nav-section1' class='n2'><a href='".$localizer."research.php'>RESEARCH</a></li>
+            <li id='main-nav-section2' class='n3'><a href='".$localizer."events.php'>EVENTS</a></li>
+            <li id='main-nav-section3' class='n4'><a href='".$localizer."facilities/index.php'>FACILITIES</a></li>
+            <li id='main-nav-section4' class='n5'><a href='".$localizer."partners.php'>PARTNERS</a></li>
+            <li id='main-nav-section5' class='n6'><a href='".$localizer."projects.php'>PROJECTS</a></li>
+            <li id='main-nav-section6' class='n7'><a href='".$localizer."publications.php'>PUBLICATIONS</a></li>
+            <li id='main-nav-section7' class='n8'><a href='".$localizer."private.php'>PRIVATE</a></li>
         </ul>
     </nav>
+    -->
 	";
     
     
