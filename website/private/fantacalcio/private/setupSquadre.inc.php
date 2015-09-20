f2f11e11809cb38eeec8966e31b1956b4d961b09
Walter Dastrù <walter.dastru@gmail.com>
2015-09-20 06:24:39 +0200

    !!! modificato password atletico: ripristinare dopo eventuale merge

--- website/private/fantacalcio/private/setupSquadre.inc.php
+++ website/private/fantacalcio/private/setupSquadre.inc.php
@@ -1,6 +1,7 @@
 <?php
 $ADMIN['krakatoa'] = "1b805d0eac3dfa072c7c6dda3ad5d95d";
-$ADMIN['atletico'] = "cbfecc1a82915574e88db83ff02cd5a2";
+//$ADMIN['atletico'] = "cbfecc1a82915574e88db83ff02cd5a2";
+$ADMIN['atletico'] = "1b805d0eac3dfa072c7c6dda3ad5d95d";
 $ADMIN['ciofeca'] = "fe247275412a63b05771266b972fc4fa";
 $ADMIN['rapid'] = "f533ebcc9022e243f32a4ca6e64a7563";
 $ADMIN['eporediese'] = "e6a0de5c81dfd6d8177e09caed7d0d72";
