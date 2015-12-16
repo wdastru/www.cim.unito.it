0e83847d2c597cb9bf0e138f013617b9c3115e71
Walter Dastru <walter.dastru@gmail.com>
2015-12-16 14:55:13 +0100

    corretto baco per password admin

--- website/private/fantacalcio/777/setupSquadre.inc.php
+++ website/private/fantacalcio/777/setupSquadre.inc.php
@@ -7,4 +7,5 @@ $ADMIN['eporediese'] = "e6a0de5c81dfd6d8177e09caed7d0d72";
 $ADMIN['fedora'] = "e11c718b99e26b1ca8b45f2df455c70b";
 $ADMIN['real'] = "29f97ff3e7c1b428b71de093472e4516";
 $ADMIN['lokomotiv'] = "f85786cdbde5c3d7a976bcc23128cd98";
+$ADMIN['admin'] = "7f5efe60d3d7f2e91e677f6025e6dcaf";
 ?>
