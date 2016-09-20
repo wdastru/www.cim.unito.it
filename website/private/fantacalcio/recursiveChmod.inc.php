70d73c502634a3550997598f7ff44dd61f88271d
Walter Dastru <walter.dastru@gmail.com>
2016-09-20 12:38:01 +0200

    ok

--- website/private/fantacalcio/recursiveChmod.inc.php
+++ website/private/fantacalcio/recursiveChmod.inc.php
@@ -31,12 +31,17 @@ function recursiveChmod($path, $filePerm = 0777, $dirPerm = 0777) {
 	} elseif (is_dir($path)) {
 		// Then get an array of the contents
 		$foldersAndFiles = scandir($path);
+                //print_r($foldersAndFiles);
 		// Remove "." and ".." from the list
 		$entries = array_slice($foldersAndFiles, 2);
+                //print_r($entries);
 		// Parse every result...
 		foreach ($entries as $entry) {
 			// And call this function again recursively, with the same permissions
-			recursiveChmod($path . "/" . $entry, $filePerm, $dirPerm);
+                        //if ($entry == ".")
+                        //	continue;
+                        //echo $entry." ";
+	        	recursiveChmod($path . "/" . $entry, $filePerm, $dirPerm);
 		}
 		// When we are done with the contents of the directory, we chmod the directory itself
 		chmod($path, $dirPerm);
