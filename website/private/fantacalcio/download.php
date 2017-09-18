618830e5b5d4a546d2a434bf8f9d493409836b98
Walter Dastru <walter.dastru@gmail.com>
2017-09-18 10:28:01 +0200

    rimosso NEW da vecchi FantaCalcGui in download.php

--- website/private/fantacalcio/download.php
+++ website/private/fantacalcio/download.php
@@ -43,8 +43,8 @@ $numeriRomani = array( "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX
 		<br />
 
 		<table id='downloads' style="margin: auto; padding: 10px;">
-      <tr><td><img class='new' src='figure/new.gif'></td><td class='left'><a href="download/FantaCalcGui_v3.4.1_Setup.exe">FantaCalcGui v3.4.1 (installer)</a></td></tr>
-      <tr><td><img class='new' src='figure/new.gif'></td><td class='left'><a href="download/FantaCalcGui_v3.4.1.zip">FantaCalcGui v3.4.1.zip</a></td></tr>
+      <tr><td><!-- <img class='new' src='figure/new.gif'> --></td><td class='left'><a href="download/FantaCalcGui_v3.4.1_Setup.exe">FantaCalcGui v3.4.1 (installer)</a></td></tr>
+      <tr><td><!-- <img class='new' src='figure/new.gif'> --></td><td class='left'><a href="download/FantaCalcGui_v3.4.1.zip">FantaCalcGui v3.4.1.zip</a></td></tr>
     </table>
 
 
