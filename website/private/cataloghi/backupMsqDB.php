245fff06b54efa005b93d28cf5beb817c7dbe961
Walter Dastru <walter.dastru@gmail.com>
2017-09-18 10:17:11 +0200

    ...

--- website/private/cataloghi/backupMsqDB.php
+++ website/private/cataloghi/backupMsqDB.php
@@ -1,8 +1,9 @@
 <?php
-    require ("connect.php");
-    require ("variables.php");
+	phpinfo();
 
+	require ("connect.php");
+    require ("variables.php");
+    
     exec('mysqldump -u cim_adm cimdb catalogo > cimdb_catalogo.sql');
     
-    exit;
 ?>
