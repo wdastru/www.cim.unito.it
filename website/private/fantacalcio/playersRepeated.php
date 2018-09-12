57200bf0be9fad76bfa1ebe6c933aee4e899ff7e
Walter Dastru <walter.dastru@gmail.com>
2018-09-12 16:21:56 +0200

    modifiche per giocatori ripetuti

--- website/private/fantacalcio/playersRepeated.php
+++ website/private/fantacalcio/playersRepeated.php
@@ -1,7 +1,19 @@
+
+<script type="text/javascript">
+    function blink_text() {
+        $('.blink').fadeOut(250);
+        $('.blink').fadeIn(250);
+    }
+    setInterval(blink_text, 1000);
+</script>
+
 <?php
 /* Creazione liste giocatori per ogni squadra */
 
 require_once  $relocate_string . 'squadre.inc'; // definito shortName[]
+
+$playersRepeated=0;
+$playersFound="";
 
 for ($j=0; $j<8; $j++)
 {
@@ -21,13 +33,21 @@ for ($j=0; $j<8; $j++)
 				//echo "B-" . $m . "-" . $giocatore[$k][$m]."-B</br>";
 				
 				if ($giocatore[$j][$i] == $giocatore[$k][$m]) {
-					echo "<span class='ripetuto'>" . $giocatore[$j][$i]. " ripetuto - " . $giocatore[$j][25] . " - " . $giocatore[$k][25] . "</span><br/>";
+				    $playersRepeated=1;
+				    $playersFound .= "<span>" . $giocatore[$j][$i]. " - " . $giocatore[$j][25] . " / " . $giocatore[$k][25] . "</span><br/>";
 				}
 			}
 		}
 	}
 }
 
+if ($playersRepeated) {
+    echo "<div id='hiddenBox' style='width:400px; margin:auto;'>";
+    echo "<span class='ripetuto blink'>ATTENZIONE presenza di giocatori in più squadre !!!</span><br/><br/>";
+    echo $playersFound;
+    echo "</div></br>";
+    $playersRepeated=0;
+}
 
 /**/
 ?>
--- website/private/fantacalcio/squadre/squadra.php
+++ website/private/fantacalcio/squadre/squadra.php
@@ -113,6 +113,10 @@ if(file_exists($fileSquadreXml))
 	<script type="text/javascript"
 		src="../documentPreProcessor.php?document=javascript.js&type=javascript"></script>
 	<!-- InstanceEndEditable -->
+	
+	<!-- InstanceBeginEditable name="jQuery" -->
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>	
 
 </head>
 <!-- <body onresize="Count();"> -->
