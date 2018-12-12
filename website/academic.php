2d3a1290457de1f112c801a34bc673758d999b36
Sara Zullino <sara.zullino@unito.it>
2018-12-12 18:08:56 +0100

    partners list updated

--- website/academic.php
+++ website/academic.php
@@ -45,6 +45,9 @@
 
 						<ul>
 							<li>Department of Biotechnology, University of Verona (Prof. Michael Assfalg)</li>
+							<li>Istituto Biostrutture e Bioimmagini, IBB-CNR, Napoli (Prof. Mancini)</li>
+							<li>Istituto di Cristallografia, CNR, Bari (Dr. M. Saviano)</li>
+							<li>Dipartimento di Scienze ed Innovazione Tecnologica, Universit&agrave; del Piemonte Orientale "A. Avogadro", Alessandria (Prof. M. Botta)</li>
 
 						</ul>
 
@@ -57,6 +60,9 @@
 							<li>Department of Biological Regulation, Weizmann Institute of Science, Rehovot (Prof. Michal Neeman)</li>
 							<li>Medical Faculty Mannheim, Heidelberg University (Prof. Frank Zoellner)</li>
 							<li>Biomedical Imaging Group, Erasmus MC - University, Rotterdam (Prof. Stefan Klein)</li>
+							<li>Institute of Technical Sciences of the Serbian Academy of Sciences and Arts, Belgrade, Serbia (Dr. Magdalena Stevanovic)</li>
+							<li>Service d'Imagerie Diagnostique et Th&eacute;rapeutique, Universit&eacute;  Claude Bernard Lyon 1, Lyon, France (Prof. P. Douek)</li>
+							<li>School of Biomedical Engineering and Imaging Sciences, King's College, London, UK (Prof. R. Botnar)</li>
 
 						</ul>
 
