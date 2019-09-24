9de7b641118b7e5cf7182ac091a2d68f1b916ea6
Walter Dastru <walter.dastru@gmail.com>
2019-09-24 15:01:28 +0200

    mod in uploader_FantaCalcGui.php

--- website/private/fantacalcio/uploader_FantaCalcGui.php
+++ website/private/fantacalcio/uploader_FantaCalcGui.php
@@ -8,8 +8,8 @@
 	if(move_uploaded_file($file_location,"$dir_file")) {
 		MySingleton::writeToLog(basename($_FILES['uploaded_file']['name']) . " saved to " . $relocate_string . "777/", $relocate_string . 
 "777/logs/uploader_FantaCalcGui.log");
-		MySingleton::writeToLog("$dir_file");
-		chmod("$dir_file", '0777');
+		MySingleton::writeToLog("$dir_file", $relocate_string . "777/logs/uploader_FantaCalcGui.log");
+		chmod($dir_file, '0777');
 	} else{
     	MySingleton::writeToLog(basename($_FILES['uploaded_file']['name']) . " could not be saved to " . $relocate_string . "777/", $relocate_string . 
 "777/logs/uploader_FantaCalcGui.log");
