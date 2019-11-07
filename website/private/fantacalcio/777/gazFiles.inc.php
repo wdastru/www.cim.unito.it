9ce94818a3aaec19057f2a48779236c7ef4b642d
Walter Dastru <walter.dastru@gmail.com>
2019-11-07 15:38:43 +0100

    777/

--- website/private/fantacalcio/777/gazFiles.inc.php
+++ website/private/fantacalcio/777/gazFiles.inc.php
@@ -1,3 +1,3 @@
 <?php
- $fileGaz=array("01.txt", "02.txt", "03.txt", "04.txt", "05.txt", "06.txt", "07.txt", ); 
+ $fileGaz=array("01.txt", "02.txt", "03.txt", "04.txt", "05.txt", "06.txt", "07.txt", "08.txt", "09.txt", "10.txt", "11.txt", ); 
 ?>
