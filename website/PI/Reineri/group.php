73bf8b23d3b512fe04d77613cf451a62604c0d44
Sara Zullino <sara.zullino@gmail.com>
2020-01-10 14:36:35 +0100

    reineri lab webpage added

---
+++ website/PI/Reineri/group.php
@@ -0,0 +1,298 @@
+<?php
+	$localizer = "../../";
+?>
+<!DOCTYPE HTML>
+<!--
+	ZeroFour by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>Group Members | REINERI Lab</title>
+	<?php require ($localizer . 'PI/Reineri/includes/head_const_reineri.php'); ?>
+</head>
+<body class="homepage">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<div id="header-wrapper">
+			
+			<!-- logo unito -->
+			<a href="http://www.unito.it" title="University of Torino" target="_blank" id="unito-logo" style="text-decoration: none;">
+			<div class="unito-logo-container">                   
+	          </div></a>
+	          
+	          <a href="http://www.cim.unito.it" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
+	          <div class="cim-logo-container">
+	         </div></a>
+			
+			
+			<div class="container">
+					
+				<!-- Header -->
+				<header id="header" style="margin: -30px 0 30px 0;">
+					<div class="inner">
+					
+						<!-- Logo -->
+						<h1>
+							<a href="<?php echo $localizer; ?>PI/Reineri/home.php" id="logo">REINERI LAB</a>
+							<div>
+								<p style="color: gray; font-size=12px;" >Imaging Tumor Microenvironment</p>
+
+							</div>
+						</h1>
+						<!-- Nav -->
+						<nav id="nav">
+							<?php require ($localizer . 'PI/Reineri/includes/menu_reineri.php'); ?>
+						</nav>
+
+					</div>
+				</header>
+
+			</div>
+		</div>
+
+
+		<!-- Main Wrapper -->
+		<div id="main-wrapper">
+
+			<div class="wrapper style3">
+				<div class="inner">
+					<div class="container">
+						<div class="row">
+							<div class="8u 10u(mobile)">
+
+								<!-- Article list -->
+								<section class="box article-list">
+									<h2 class="second icon fa-user">Group Members</h2>
+									</section>
+									</div>
+									</div>
+
+									<!-- Excerpt -->
+									<div class="row">
+							<div class="8u 12u(mobile)">
+									<article class="box excerpt">
+										<img
+											src="<?php echo $localizer; ?>PI/Reineri/images/reineri.jpg" alt="" style="width: 180px; height: auto; margin-bottom:80px;" class="image left"/>
+										<div class="w3-row">
+										<div class="w3-threequarter">
+											<header>
+												<h3>Francesca Reineri</h3>
+												<span><strong>Principal Investigator</strong></span>
+											</header>
+											<p style="line-height: 1.2">
+												Associate Professor<br> Molecular Imaging Center<br>
+												Academic activity: <a href="https://www.dmbhs.unito.it/do/docenti.pl/Alias?francesca.reineri#tab-profilo" target=_blank>Dept. Molecular Biotechnology and Health Sciences</a><br>
+												Ph.D. Chemistry, University of Torino, Italy<br>  
+												M.Sc. Chemistry, University of Torino, Italy<br>
+												Phone: +39 0116706473<br> 
+												E-mail: <a href="mailto:francesca.reineri@unito.it">francesca.reineri@unito.it</a></p>											
+											</div>									
+									
+										</div>
+									</article>
+									</div>
+									<div class="4u 6u(mobile)">
+									
+												<br><h4 style="margin: 0 0 0 4em;"">Research Topics</h4>
+											<img src="<?php echo $localizer; ?>PI/Reineri/images/reineri_wordcloud.png" class="w3-image"
+									style="max-width: 250px;" />
+									
+									</div>
+									</div>
+									
+									<hr></hr>
+									
+									
+										<div class="row">
+							<div class="8u 12u(mobile)">
+									
+									<!-- Excerpt -->
+									<article class="box excerpt">
+										<img src="<?php echo $localizer; ?>PI/Reineri/images/cavallari.jpg" alt="" style="width: 180px; height: auto; margin-bottom:60px;" class="image left"/>
+										<div>
+											<header>
+												<h3>Eleonora Cavallari</h3>
+												<span>Postdoctoral Fellow</span>
+											</header>
+											<p style="line-height: 1.2">
+												PhD Pharmaceutical and Biomolecular Sciences, University of Torino, Italy<br>
+												MSc Physics, University of Torino, Italy<br> Phone: +39
+												0116706452<br> E-mail: <a
+													href="mailto:eleonora.cavallari@unito.it">eleonora.cavallari@unito.it</a>
+											</p>
+										</div>
+										
+										<div class="showtext">
+										<div>My project focuses on ...</div>
+										<form> <p align="justify"><b>My research is focused to the development of the parahydrogen-based hyperpolarization method named PHIP-Said 
+										Arm Hydrogenation (PHIP-SAH), to the visualization of metabolic processes in real time and the detection of altered metabolic conditions in 
+										several diseases. I am particularly interested in improving the heteronuclear polarization transfer and whole production process of hyperpolarized 
+										(HP) imaging probes. My ultimate goal is to refine this promising diagnostic tool that gives the opportunity of a wide application of 
+										HP molecules to medical research.
+
+												
+												</b>									
+											</p>
+										</form>
+										</div>
+										
+									</article>
+									</div>
+									<div class="4u 6u(mobile)">
+									
+												<br><h4 style="margin: 0 0 0 4em;"">Research Topics</h4>
+											<img src="<?php echo $localizer; ?>PI/Reineri/images/cavallari_wordcloud.png" class="w3-image"
+									style="max-width: 250px;" />
+									
+									</div>
+									</div>
+									
+									<hr></hr>
+									
+									<div class="row">
+							<div class="8u 12u(mobile)">
+									
+									<!-- Excerpt -->
+									<article class="box excerpt">
+										<img src="<?php echo $localizer; ?>PI/Reineri/images/carrera.jpg" alt="" style="width: 180px; height: auto; margin-bottom:60px;" class="image left" />
+										<div>
+											<header>
+												<h3>Carla Carrera</h3>												
+												<span>Senior Research Scientist</span>
+											</header>
+											<p style="line-height: 1.2">
+												PhD Pharmaceutical and Biomolecular Sciences, University of Torino, Italy<br>
+												MSc Pharmaceutical Chemistry, University of Torino, Italy <br> Phone: +39 0116706493<br>
+												E-mail: <a href="mailto:carla.carrera@unito.it">carla.carrera@unito.it</a>
+											</p>
+										</div>
+										
+										<div class="showtext">
+										<div>My research aims at ...</div>
+										<form> <p align="justify"><b>
+										My research focuses on the design and synthesis of new contrast agents for MRI and their charaterization. My expertise covers the synthesis, among others, 
+										of Lantanide Complexes used as classic imaging probes, and innovative Hyperpolarized molecules exploited to visualize in real time the metabolic transformations 
+										of biomolecules in physiological and pathological pathways. I am personally involved in the complete workflow, starting from the synthesis and purification of 
+										the molecules, their charaterization by means of HPLC, Mass and NMR spectoscopy, and their use as molecular probes for in vitro and in vivo MRI experiments.</a>
+												</b>									
+											</p>
+										</form>
+										</div>
+										
+									</article>
+									</div>
+									<div class="4u 6u(mobile)">
+									
+												<br><h4 style="margin: 0 0 0 4em;"">Research Topics</h4>
+											<img src="<?php echo $localizer; ?>PI/Reineri/images/carrera_wordcloud.png" class="w3-image"
+									style="max-width: 250px;" />
+									
+									</div>
+									</div>
+									
+									<hr></hr>
+																	
+									
+									<div class="row">
+							<div class="8u 12u(mobile)">
+															
+									<!-- Excerpt -->
+									<article class="box excerpt">
+										<img src="<?php echo $localizer; ?>PI/Reineri/images/bondar.jpg" alt="" style="width: 180px; height: auto; margin-bottom:60px;" class="image left" />
+										<div>
+											<header>
+												<h3>Oksana Bondar</h3>												
+												<span>PhD Student</span>
+											</header>
+											<p style="line-height: 1.2">
+												Inorganic chemistry, Taras Shevchenko National University of Kyiv, Ukraine
+												<br> Phone: +39 0116706493<br>
+												E-mail: <a href="mailto:oksana.bondar@unito.it">oksana.bondar@unito.it</a>
+											</p>
+										</div>
+										
+										<div class="showtext">
+										<div>My research focuses on ...</div>
+										<form> <p align="justify"><b>
+										Early stage researcher (PhD student), within the ZULF project, at the Molecular Imaging Centre . My scientific experience is organic and inorganic synthesis. 
+										My current project is devoted to the development of the hyperpolarization method named PHIP-SAH (ParaHydrogen Induced Polarization- Side Arm Hydrogenation) 
+										and on the ZULF NMR detection of these hyperpolarized molecule of metabolites. 
+												</b>									
+											</p>
+										</form>
+										</div>
+										
+									</article>
+									</div>
+									
+									</div>
+									
+									<hr></hr>
+									
+									
+									<hr></hr>
+									
+
+								
+								<div class="row">
+							<div class="8u 12u(mobile)">
+								<article class="box excerpt">
+								<div>
+									<h2 class="second icon fa-user">Lab Alumni</h2>
+									
+									<ul class="divided">
+											<li><b>Martina Maltone</b>, MSc student in Molecular Biotechnologies</li>
+
+											
+										</ul>
+									</div>
+									</article>
+									</div>
+									</div>
+									
+									<hr></hr>
+									
+									<div class="row">
+							<div class="12u 12u(mobile)">
+								<article class="box excerpt">
+								<div>
+									<h2 class="second icon fa-user">Master Thesis Opportunities</h2>
+									
+									<p> We are offering master thesis for students registered with the Faculty of Biotechnology, Pharmacy, Physics, Bioengineering.<br>
+									A Master thesis (duration: at least 6 months) consists of a small scientific project, under the supervision of a postdoc or an advanced PhD student
+									in the fields of cancer biology - pharmaceutics - in vivo imaging - image processing.</p>
+									
+									</div>
+									</article>
+									</div>
+								
+								
+							
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+
+	<!-- Scripts -->
+
+	<script src="<?php echo $localizer; ?>PI/assets/js/jquery.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/jquery.dropotron.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/skel.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/skel-viewport.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="<?php echo $localizer; ?>PI/assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="<?php echo $localizer; ?>PI/assets/js/main.js"></script>
+	<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+	<script>
+</script>
+
+</body>
+</html>
