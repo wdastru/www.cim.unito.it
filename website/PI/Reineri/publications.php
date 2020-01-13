a991a1cff29a660df7ed1e2f72d9d7d82f1e7355
Sara Zullino <sara.zullino@gmail.com>
2020-01-13 18:13:45 +0100

    reineri publications moved

---
+++ website/PI/Reineri/publications.php
@@ -0,0 +1,95 @@
+<?php
+	$localizer = "../../";
+?>
+<!DOCTYPE HTML>
+<!--
+	ZeroFour by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Publications | REINERI Lab</title>
+    <?php require ($localizer . 'PI/Reineri/includes/head_const_reineri.php'); ?>
+</head>
+<body class="homepage">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<div id="header-wrapper">
+			
+			<!-- logo unito -->
+			<a href="http://www.unito.it" title="University of Torino" target="_blank" id="unito-logo" style="text-decoration: none;">
+			<div class="unito-logo-container">                   
+	          </div></a>
+	          
+	          <a href="http://www.cim.unito.it" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
+	          <div class="cim-logo-container">
+	         </div></a>
+			
+			
+			
+			<div class="container">
+					
+				<!-- Header -->
+				<header id="header" style="margin: -30px 0 30px 0;">
+					<div class="inner">
+					
+						<!-- Logo -->
+						<h1>
+							<a href="<?php echo $localizer; ?>PI/Reineri/home.php" id="logo">REINERI LAB</a>
+							<div>
+								<p style="color: gray; font-size=12px;" >Hyperpolarization - ParaHydrogen - Metabolism</p>
+
+							</div>
+						</h1>
+						<!-- Nav -->
+						<nav id="nav">
+							<?php require ($localizer . 'PI/Reineri/includes/menu_reineri.php'); ?>
+						</nav>
+
+					</div>
+				</header>
+
+			</div>
+		</div>
+
+		<!-- Main Wrapper -->
+		<div id="main-wrapper">
+			<div class="wrapper style1">
+				<div class="inner">
+					<div class="container">
+						<div class="row">
+
+							<div class="8u 12u(mobile)">
+
+							<!-- Content -->
+							<article>								
+									<h2 class="icon fa-file-text-o">Journal Papers</h2>
+									<ol class="default" reversed=true>
+									<div style="line-height: 1.5">
+										<p>Full list of publications are available at 
+										<a title="Open in a new window" href="https://scholar.google.it/citations?user=mJK0H8cAAAAJ&hl=it" target="_blank">Google Scholar</a>
+										</p>
+									</div>
+								</ol>
+							</article>				
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>			
+	</div>			
+
+	<!-- Scripts -->
+
+	<script src="<?php echo $localizer; ?>PI/assets/js/jquery.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/jquery.dropotron.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/skel.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/skel-viewport.min.js"></script>
+	<script src="<?php echo $localizer; ?>PI/assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="<?php echo $localizer; ?>PI/assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="<?php echo $localizer; ?>PI/assets/js/main.js"></script>
+
+</body>
+</html>
