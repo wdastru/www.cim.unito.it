ff0a8dbe1fdbf1c79c731526e320211155f84be0
Sara Zullino <sara.zullino@gmail.com>
2020-01-15 15:01:25 +0100

    research_2 added in dandrea webpage

--- website/PI/DAndrea/research_2.php
+++ website/PI/DAndrea/research_2.php
@@ -9,15 +9,15 @@
 -->
 <html>
 <head>
-	<title>Kidney Imaging | Longo Lab</title>
-	<?php require ($localizer . 'PI/Longo/includes/head_const_longo.php'); ?>
+	<title>Peptide Modification | D'Andrea Lab</title>
+	<?php require ($localizer . 'PI/DAndrea/includes/head_const_dandrea.php'); ?>
 </head>
 <body class="homepage">
 	<div id="page-wrapper">
 
 		<!-- Header -->
 		<div id="header-wrapper">
-
+			
 			<!-- logo unito -->
 			<a href="http://www.unito.it" title="University of Torino" target="_blank" id="unito-logo" style="text-decoration: none;">
 			<div class="unito-logo-container">                   
@@ -26,25 +26,25 @@
 	          <a href="http://www.cim.unito.it" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
 	          <div class="cim-logo-container">
 	         </div></a>
-
-
+			
+			
 			<div class="container">
-
+					
 				<!-- Header -->
 				<header id="header" style="margin: -30px 0 30px 0;">
 					<div class="inner">
-
+					
 						<!-- Logo -->
 						<h1>
-							<a href="<?php echo $localizer; ?>/PI/Longo/home.php" id="logo">DARIO LONGO LAB</a>
+							<a href="<?php echo $localizer; ?>/PI/DAndrea/home.php" id="logo">Luca D'Andrea LAB</a>
 							<div>
-								<p style="color: gray;">Imaging Tumor Microenvironment</p>
+								<p style="color: gray; font-size=12px;" >Peptides and proteins for diagnostic and therapeutic applications</p>
 
 							</div>
 						</h1>
 						<!-- Nav -->
 						<nav id="nav">
-							<?php require ($localizer . 'PI/Longo/includes/menu_longo.php'); ?>
+							<?php require ($localizer . 'PI/DAndrea/includes/menu_dandrea.php'); ?>
 						</nav>
 
 					</div>
@@ -64,28 +64,18 @@
 
 							<article>
 
-								<h3 font style="text-transform: none;">Imaging renal pH</h3>
-								<p align="justify">
-									The kidney is a highly complex organ consisting of welldefined
-									structures that function in a deeply coordinate
-									fashion to allow for fine regulation of pH homeostasis.
-									The role of the kidney in acid-base balance depends
-									on the capacity of the renal tubule to reclaim filtered
-									bicarbonate and to excrete net protons as titratable acids
-									and ammonium.</br>
-									Although the principal role of the kidney is the maintenance of
-									acid–base balance, current imaging approaches are unable to
-									assess this important parameter and clinical biomarkers are not
-									robust enough in evaluating the severity of kidney damage.
-									Therefore, our lab is developing novel noninvasive imaging approaches
-									to assess the acid–base homeostasis in vivo and to monitor pH evolution following kidney injury. 
-									Our lab is also actively involved in the <b>COST Action "PARENCHIMA"</b> 
-									<a	href="http://www.cost.eu/COST_Actions/ca/CA16103"
-										target="_blank"><font color="blue">CA16103 </font></a>
-									(Magnetic Resonance Imaging Biomarkers for Chronic Kidney
-									Disease).<br><strong> We aim to demonstrate the biological validity
-									of renal pH imaging as a novel biomarker of kidney diseases.</strong>
+								<h3 font style="text-transform: none;">Site-selective modification of peptides and proteins</h3>
+								
+								<p align="justify">The ability to selectively modify a peptide/protein molecule opens the way to a plethora of novel 
+								applications for the target biomolecule and helps in understanding its structural/functional role.  Proteins can be 
+								modified introducing unnatural amino acids, fluorophores, labeled atoms/residues, post-translation modifications, 
+								chelating units, polymers, nanoparticles, ect. The site-selective modifications are introduced using chemo-selective 
+								reactions, such as chemical ligation, in order to obtain a homogenous product. We are especially interested in the 
+								bioconjugation of the molecules of interest with imaging probes for application in optical and medical imaging.
 								</p>
+
+								<div style="text-align: center;">
+									<img src="<?php echo $localizer; ?>PI/DAndrea/images/figure_research_overview_2.jpg" style="max-width: 50%; height: auto;"><br/></div>
 
 
 							</article>
@@ -98,47 +88,22 @@
 									<article>
 
 										<header>
-											<h3 font style="text-transform: none;">
-												<font color="red">Renal pH imaging</font></a>
-											</h3>
-											</br>
-											<p font style="text-transform: none; font-size:1.3em">Imaging the pH evolution in an acute kidney injury model </p>
-											</br>
+											<p font style="text-transform: none; font-size:1.3em">VEGFR1D2 bioconjugation: a tool for VEGF targeting</p>
 										</header>
-										<img src="<?php echo $localizer; ?>/PI/Longo/images/kidney1.jpg" alt="" class="image zooming">
+										<img src="<?php echo $localizer; ?>/PI/DAndrea/images/research_2_1.jpg" alt="" class="image zooming">
 
 										<p align="justify">
-											Our previous studies have shown the ability of a
-											iodinated CA, Iopamidol, to measure pH values and
-											induced pH alterations in healthy mice 
-											<a class="html" href="https://www.ncbi.nlm.nih.gov/pubmed/20949634"
-												target="_blank"><font color="blue">[Longo et al. 2011]</font></a>. 
-											In this study we employed the glycerol-induced AKI model due to the
-											multiple ischemic, toxic, and obstructive tubular insults
-											caused, pathophysiologically similar to the acute tubular
-											necrosis that occurs in humans. According to the
-											elicited damage, this model shows an inherent capability
-											to recover from the damage, therefore allowing to image
-											pH variations both at the acute stage of the injury and
-											during the recovery period. We observed an increase
-											of pH levels reaching a peak at day 3 that correlates well
-											with the corresponding BUN raise and a complete recovery 21 days following the damage
-											<a class="html" href="https://www.ncbi.nlm.nih.gov/pubmed/23059893"
-												target="_blank"><font color="blue">[Longo et al. 2013]</font></a>.
-										
+										We report the use of a chemical ligation based strategy for the site-selective and covalent modification 
+										of the second domain of VEGFR1D2, an Ig-like domain directly involved in the ligand binding in vivo. 
+										VEGF/VEGFR biological system is the main regulator of angiogenesis, a physiological process controlling 
+										the sprouting of new blood vessels from existing ones and whose impairment contributes to the onset, 
+										development and progression of several diseases. VEGF and its receptors are considered excellent candidates 
+										for the monitoring and the treatment of pathological angiogenesis. We have developed a bioconjugation strategy, 
+										based on the use of an oxime-ligation reaction, to site-selectively label the VEGFR1D2 N-terminal site. 
+										The N-terminal serine residue was selectively oxidized to aldehyde and then a molecular probe harboring 
+										an alkoxyamine function was conjugated. VEGFR1D2 suitably modified with convenient molecular probes can 
+										be used as a tool for VEGF targeting with both diagnostic and therapeutic applications.																					 												
 										</p>
-										<img src="<?php echo $localizer; ?>/PI/Longo/images/kidney2.jpg" alt="" class="image zooming">
-										<p align="justify">We have demonstrated that the use of Iopamidol
-											as a CEST-MRI pH responsive CA provides a
-											good estimate of the kidney pH evolution both in healthy
-											and in pathological conditions. CEST-MRI pH imaging is
-											a suitable method for the noninvasive and longitudinal
-											investigation of renal injury and may provide useful
-											insight on monitoring functional recovery. Moreover, the
-											proposed method provides spatial information on single
-											kidney function and integrity, and not just an average of
-											the kidneys’ functionality as with clinical biomarker
-											such as serum creatinine and BUN.</p>
 										
 									</article>
 								</section>
@@ -152,37 +117,32 @@
 									<article>
 
 										<header>
-											<h3 font style="text-transform: none;">
-												<font color="red">Single kidney functional pH imaging</font></a>
-											</h3>
-											
-											<p font style="text-transform: none; font-size:1.3em">Evaluation of renal pH homeostasis after ischemia reperfusion injury</p>
+											<p font style="text-transform: none; font-size:1.3em">Site-specific protein double labeling by expressed protein ligation: applications to repeat proteins</p>
 										</header>
-										<img src="<?php echo $localizer; ?>/PI/Longo/images/kidney3.jpg" alt="" class="image zooming">
 
 										<p align="justify">
-											Ischemic renal injury is a severe clinical problem in nephrology and the
-											major cause of acute kidney injury (AKI). We investigated whether MRI-CEST renal pH mapping
-											can detect early renal damage and distinguish AKI recovery from irreversible damage
-											in a unilateral kidney ischemia reperfusion injury (KIRI) model. The advantage of this model is that 
-											only one kidney is damaged, whereas the contralateral kidney is not affected. Consequently, 
-											in this model of post-ischemic AKI, serum biomarkers are minimally affected, since the contralateral
-											kidney can compensate for the reduced renal functionality.
-											We shown that MRI-CEST pH mapping can detect the early onset of acute
-											kidney damage and distinguish between the recovery and persistence
-											of the damage following unilateral ischemia reperfusion injury in mice.
-										<a class="html" href="https://www.ncbi.nlm.nih.gov/pubmed/28370530" target="_blank"><font color="blue">[Longo et al. 2017]</font></a>.
-											Morover, MRI-CEST renal pH values were significantly correlated
-											with histological scores, reflecting renal damage.
-										</p>
-										<img src="<?php echo $localizer; ?>/PI/Longo/images/kidney4.jpg" alt="" class="image zooming">
-										<p align="justify">The results reported herein show, for the first time, that MRI-CEST
-											pH mapping allows the noninvasive detection and monitoring of
-											renal function impairment after unilateral ischemia-induced AKI.
-											The derangement of pH regulation was correlated to the decay
-											of renal filtration function and to the severity of the morphological
-											damage.</p>
-										
+										We reported a semi-synthetic strategy based on expressed protein ligation to prepare proteins, in high purity and homogeneity, with 
+										two different molecular probes incorporated specifically at any desired position
+										<a class="html" href="https://pubs.rsc.org/en/content/articlelanding/2012/ob/c1ob06397a#!divAbstract"
+												target="_blank"><font color="blue">[De Rosa et al. 2012]</font></a>. 
+										Remarkably, this approach can be adapted to the introduction of more than two molecular probes. We applied this methodology to 
+										the preparation of different versions of cTPR, a designed repeat protein
+											<a class="html" href="https://www.ncbi.nlm.nih.gov/pubmed/12737816"
+												target="_blank"><font color="blue">[Main et al. 2003]</font></a>, 
+										double labeled with fluorophores in order to analyze its folding mechanism by single molecules FRET (smFRET). The protein was split in two 
+										halves. The N-terminal fragment was expressed as intein construct. The C-terminal fragment was prepared by recombinant expression or solid 
+										phase peptide synthesis. After intein splicing the N-terminal fragment was labeled in solution on the single Cys residue using 
+										the maleimide-thiol chemistry. Then, the two fragments were ligated by a native chemical ligation reaction and the junction-site Cys 
+										was exploited for the second labeling (maleimide-thiol). We made four different cTPR variants which were analyzed by smFRET 										
+										<a class="html" href="https://www.ncbi.nlm.nih.gov/pubmed/26207891"
+												target="_blank"><font color="blue">[Cohen et al. 2015]</font></a>. 
+												Based on this study we highlighted on the molecular basis for the elasticity of repeat proteins 
+										in general and TPRs in particular, demonstrating that it can be modulated by weakening hydrophobic interactions at the interfaces between repeat units. 
+											
+										 <img src="<?php echo $localizer; ?>/PI/DAndrea/images/research_2_2_1.jpg" alt="" class="image zooming">
+										 <img src="<?php echo $localizer; ?>/PI/DAndrea/images/research_2_2_2.jpg" alt="" class="image zooming">
+											
+											
 									</article>
 								</section>
 							</div>
