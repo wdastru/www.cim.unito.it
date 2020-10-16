c8a06100b4730322983b19c5f5aadce1cbf35f2b
Walter Dastru <walter.dastru@gmail.com>
2020-10-16 12:21:49 +0200

    fixed PI Terreno

--- website/PI/Terreno/research_1.php
+++ website/PI/Terreno/research_1.php
@@ -10,6 +10,7 @@
 <html>
 <head>
 	<title>Targeting imaging probes | TERRENO Lab</title>
+	<?php require ($localizer . 'includes/head_const.inc.php'); ?>
 	<?php require ($localizer . 'PI/Terreno/includes/head_const_terreno.php'); ?>
 </head>
 <body class="homepage">
