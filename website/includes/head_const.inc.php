9a9ede0e869afe3e75062ca332eb1bd6aa2a36c6
Walter Dastru <walter.dastru@gmail.com>
2020-10-20 15:07:24 +0200

    fixed css bugs

--- website/includes/head_const.inc.php
+++ website/includes/head_const.inc.php
@@ -6,10 +6,12 @@ echo "
 
 	<meta name='description' content='University of Torino. Website of the Molecular Imaging Center.' />
     <meta name='google-site-verification' content='q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0' />
-    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
     <meta http-equiv='Cache-control' content='public' />
     <meta name='keywords' content='NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CIM, Torino, Italy, Liposomi, risonanza, magnetica, Italia' />
+    <meta name='robots' content='index, follow'>
+    <meta name='revisit-after' content='1 week'>
     <link href='" . $localizer . "stylesheet.css' rel='stylesheet' type='text/css' />
     <link href='" . $localizer . "images/favicon.gif' rel='icon' type='image/gif' />
     <script src='" . $localizer . "jquery.cycle2.min.js' type='text/javascript'></script>
