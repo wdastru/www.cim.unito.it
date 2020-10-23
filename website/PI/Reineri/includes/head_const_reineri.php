5703637b160941d1a583cb6db670bac88281c7a1
Walter Dastru <walter.dastru@gmail.com>
2020-10-23 12:44:43 +0200

    fixed head_const_*.php

--- website/PI/Reineri/includes/head_const_reineri.php
+++ website/PI/Reineri/includes/head_const_reineri.php
@@ -1,28 +1,29 @@
 <?php 
 
-echo "
-    <meta charset='utf-8' />
-    <meta name='MobileOptimized' content='width'>
-    <meta name='HandheldFriendly' content='true'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    
-    <meta name='description'
-        content='We are interested in developing novel MRI contrast agents (Gd-based, CEST) for assessing tumor microenvironment (vascularization, acidosis) properties.' />
-        <meta name='keywords'
-            content='molecular imaging, tumor, gadolinium, contrast agent, X-ray, radiographic, kidney, acidosis, metabolism, acute kidney injury, angiogenesis, MRI, magnetic resonance imaging, CEST, vascularization' />
-            <meta name='robots' content='index, follow'>
-            <meta name='revisit-after' content='1 week'>
-            
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src='https://www.googletagmanager.com/gtag/js?id=UA-112490720-1'></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                
-                gtag('config', 'UA-112490720-1');
-            </script>
-            
+//echo "
+//    <meta charset='utf-8' />
+//    <meta name='MobileOptimized' content='width'>
+//    <meta name='HandheldFriendly' content='true'>
+//    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+//    
+//    <meta name='description'
+//        content='We are interested in developing novel MRI contrast agents (Gd-based, CEST) for assessing tumor microenvironment (vascularization, acidosis) properties.' />
+//        <meta name='keywords'
+//            content='molecular imaging, tumor, gadolinium, contrast agent, X-ray, radiographic, kidney, acidosis, metabolism, acute kidney injury, angiogenesis, MRI, magnetic resonance imaging, CEST, vascularization' />
+//            <meta name='robots' content='index, follow'>
+//            <meta name='revisit-after' content='1 week'>
+//            
+//            <!-- Global site tag (gtag.js) - Google Analytics -->
+//            <script async src='https://www.googletagmanager.com/gtag/js?id=UA-112490720-1'></script>
+//            <script>
+//                window.dataLayer = window.dataLayer || [];
+//                function gtag(){dataLayer.push(arguments);}
+//                gtag('js', new Date());
+//                
+//                gtag('config', 'UA-112490720-1');
+//            </script>";
+
+echo "     
             <!-- imaging, x-ray, tumor -->
             <!--[if lte IE 8]><script src='" . $localizer . "PI/assets/js/ie/html5shiv.js'></script><![endif]-->
             <link rel='stylesheet' href='" . $localizer . "PI/assets/css/main.css' />
