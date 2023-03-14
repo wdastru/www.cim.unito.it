70a972820ba4f1c02ca7ad2d49d551ad437634a1
Walter Dastrù <walter.dastru@gmail.com>
2023-03-14 12:13:10 +0100

    added webinar to events/educational/

---
+++ website/events/educational/data_management.php
@@ -0,0 +1,179 @@
+<?php
+$localizer = "../../";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- InstanceBegin template="/Templates/events.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+        <?php require $localizer . 'includes/head_const.inc.php'?>
+        <meta name='description'
+	content='1st Webinar on Data Management in Biomedical Imaging'>
+<title>1st Webinar on Data Management in Biomedical Imaging</title>
+<style>
+.supporters_table {
+	width: 100%;
+	table-layout: fixed;
+	width: 100%;
+}
+
+.supporters_table_cell {
+	text-align: center;
+}
+
+.supporters_table_cell img {
+	vertical-align: middle;
+	max-width: 90%;
+	height: auto;
+	margin: 20px 0px;
+	height: auto
+}
+</style>
+</head>
+<body>
+	<div id='section2'>
+            <?php
+            require ($localizer . 'includes/main-nav.php');
+            ?>
+            <div id='header'></div>
+		<!-- InstanceBeginEditable name="subsection opening" -->
+		<div id='subsection1'>
+			<!-- InstanceEndEditable -->
+			<div id='sidebar'>
+				<div class='padding'>
+					<ul id='list'>
+						<li id='section2-subsection1' class='subsection_link'><a
+							href='<?php echo $localizer;?>educational.php'>Educational</a></li>
+						<li id='section2-subsection2' class='subsection_link'><a
+							href='<?php echo $localizer;?>news.php'>News</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id='content'>
+				<div class='paddingOuter'>
+					<div class='band'>
+						<h1 class='subsectionTitle'>
+							<!-- InstanceBeginEditable name="subsection title" -->
+							1<sup>st</sup> Webinar on Data Management in Biomedical Imaging
+							<!-- InstanceEndEditable -->
+						</h1>
+					</div>
+					<div class='paddingInner'>
+						<p>During this two-part event, participants will explore various
+							topics including the visualization of large datasets, management,
+							and sharing of data and metadata, utilization of image processing
+							tools, and data repositories.</p>
+						<img src="image.jpg" style="margin: auto; width: 100%;"
+							alt="image 1">
+						<p>The webinar will provide an overview of XNAT platform, which is
+							one of the most popular open-source software platforms for
+							imaging research. The second topic of the webinar will be XNAT
+							for Preclinical Imaging Centers (XNAT-PIC), a free and
+							open-source Windows desktop application, which offers several
+							tools to expand the XNAT core functionalities to support the
+							preclinical imaging community and to promote open science
+							practices. There will be a panel where preclinical metadata and
+							its management from the point of view of ontology such as web
+							application and database archiving will be discussed.</p>
+						<p>The webinar aims to showcase the tools developed to improve the
+							discoverability, access, interoperability, and reusability of
+							preclinical image datasets, consequently providing a solid step
+							towards the adoption of the FAIR principles by the of our imaging
+							community.</p>
+
+						<h2>Speakers</h2>
+						<ul>
+							<li>Kranthi Thej Kandula Research Fellow at University of Turin
+								M.Sc. in computer science from Sapienza University</li>
+							<li>Francesco Gammaraccio Research Fellow at University of Turin
+								M.Sc. in biomedical engineering from Polytechnic University of
+								Turin.
+							
+							<li>Elisabetta Spinazzola Research Fellow at University of Turin
+								M.Sc. in biomedical engineering from Polytechnic University of
+								Turin</li>
+						</ul>
+						<h2>Details</h2>
+						<ul>
+							<li>Date: Thursday, June 15, 2023</li>
+							<li>Time: 9:30</li>
+							<li>How long: 3 hours</li>
+							<li>Language: English</li>
+							<li><a target="_blank"
+								href="https://docs.google.com/forms/d/e/1FAIpQLSeRmUl3DC0GRG6dnurp9P_5b9t7wSndOejHa3QI9MQWGlTREA/viewform?vc=0&c=0&w=1&flr=0"><span
+									style="font-weight: bold; color: #db6d16;">Register here!</span></a></li>
+						</ul>
+						<h2>Agenda</h2>
+						<h3 style="text-align: left;">SESSION 1:</h3>
+						<div style="margin-left: 20px;">
+							<h4>Theory</h4>
+							<ul>
+								<li>Biomedical Imaging Platform and Tools</li>
+								<li>XNAT for Preclinical Imaging Centers</li>
+								<li>Metadata management and storage</li>
+							</ul>
+						</div>
+						<h3 style="text-align: left;">SESSION 2:</h3>
+						<div style="margin-left: 20px;">
+							<h4>Pratical Demo</h4>
+							<ul>
+								<li>XNAT: Imaging informatics software platform</li>
+								<li>XNAT-PIC: Desktop application</li>
+								<li>Image processing on XNAT</li>
+								<li>PIDAR: Preclinical Image Dataset Repository</li>
+							</ul>
+						</div>
+						<h2>Contact us!</h2>
+						<ul>
+							<li><a href="mailto:kranthithej.kandula@unito.it">kranthithej.kandula@unito.it</a></li>
+							<li><a href="mailto:francesco.gammaraccio@unito.it">francesco.gammaraccio@unito.it</a></li>
+							<li><a href="mailto:elisabetta.spinazzola@unito.it">elisabetta.spinazzola@unito.it</a></li>
+						</ul>
+						<h2>Supported by:</h2>
+						<table class="supporters_table">
+							<tr>
+								<td class="supporters_table_cell"><a href="https://www.unito.it/"><img
+										alt="Unito logo" src="unito.png"></a></td>
+								<td class="supporters_table_cell"><a href="https://www.dbmss.unito.it/do/home.pl"><img
+										alt="DBMSS logo" src="DBMSS.png"></a></td>
+								<td class="supporters_table_cell"><a href="https://www.eurobioimaging.eu/"><img
+										alt="EuroBioImaging logo" src="eurobioimaging.png"></a></td>
+								<td class="supporters_table_cell"><a href="https://www.cnr.it/en"><img alt="CNR logo"
+										src="cnr.jpg"></a></td>
+							</tr>
+							<tr>
+								<td class="supporters_table_cell"><a href="https://healthycloud.eu/"><img
+										alt="HealthyCloud logo" src="healthycloud.png"></a></td>
+								<td class="supporters_table_cell"><a href="https://www.eosc-life.eu/"><img
+										alt="eosc-life logo" src="eosc-life.png"></a></td>
+								<td class="supporters_table_cell"><a href="https://eosc4cancer.eu/"><img
+										alt="eosc-cancer logo" src="eosc-cancer.png"></a></td>
+								<td class="supporters_table_cell"><a href="https://european-union.europa.eu/index_en"><img alt="EU flag"
+										src="eu.jpg"></a></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- subsection closing -->
+		</div>
+		<!-- subsection closing -->
+		<div id='after'></div>
+		<!-- Footer Wrapper -->
+		<div id='footer'>
+			<p>
+				Molecular Imaging Center - Via Nizza, 52 - I-10125 Torino - ITALY <br>
+				Fax. Tel. Mail
+			</p>
+			<?php include $localizer . 'includes/HTML5_badge_valid.inc.php';?>
+		</div>
+		<!-- section closing -->
+	</div>
+	<!-- section closing -->
+	<script>
+		//<![CDATA[
+		changeSideNavStyles();
+		//]]>
+	</script>
+	<!-- InstanceEnd -->
+</body>
+</html>
