9515252c38a90a4096dd0c01113a85463ef6cf02
Walter Dastrù <walter.dastru@gmail.com>
2023-05-30 16:28:46 +0200

    removed private folders fantacalcio/ prenotazioni/ autodip/

--- website/private/fantacalcio/include/title.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "<div id='title'>FANTACALCIO NMR 2019/20</div>";
-?>
--- website/private/fantacalcio/writeEmptyGazFile.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$handle = fopen('777/gazFiles.inc.php', 'w');
-	if ($handle) {
-		fwrite($handle, '<?php $fileGaz=array(); ?>');
-	}
-	fclose($handle);
-?>
