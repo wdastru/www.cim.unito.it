42624b91d9a1b556d88f19255e2b31bb87c876d9
Walter Dastrù <walter.dastru@gmail.com>
2023-07-07 15:32:33 +0200

    added online webinar link in data_management.php

--- website/events/educational/data_management.php
+++ website/events/educational/data_management.php
@@ -6,7 +6,8 @@ $localizer = "../../";
 <!-- InstanceBegin template="/Templates/events.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
         <?php require $localizer . 'includes/head_const.inc.php'?>
-        <meta name='description'
+        <meta
+	name='description'
 	content='1st Webinar on Data Management in Biomedical Imaging'>
 <title>1st Webinar on Data Management in Biomedical Imaging</title>
 <style>
@@ -41,10 +42,14 @@ $localizer = "../../";
 			<div id='sidebar'>
 				<div class='padding'>
 					<ul id='list'>
-						<li id='section2-subsection1' class='subsection_link'><a
-							href='<?php echo $localizer;?>educational.php'>Educational</a></li>
-						<li id='section2-subsection2' class='subsection_link'><a
-							href='<?php echo $localizer;?>news.php'>News</a></li>
+						<li
+							id='section2-subsection1'
+							class='subsection_link'><a
+								href='<?php echo $localizer;?>educational.php'>Educational</a></li>
+						<li
+							id='section2-subsection2'
+							class='subsection_link'><a
+								href='<?php echo $localizer;?>news.php'>News</a></li>
 					</ul>
 				</div>
 			</div>
@@ -53,7 +58,8 @@ $localizer = "../../";
 					<div class='band'>
 						<h1 class='subsectionTitle'>
 							<!-- InstanceBeginEditable name="subsection title" -->
-							1<sup>st</sup> Webinar on Data Management in Biomedical Imaging
+							1<sup>st</sup> Webinar on Data Management of Preclinical Image
+							Datasets
 							<!-- InstanceEndEditable -->
 						</h1>
 					</div>
@@ -62,7 +68,9 @@ $localizer = "../../";
 							topics including the visualization of large datasets, management,
 							and sharing of data and metadata, utilization of image processing
 							tools, and data repositories.</p>
-						<img src="image.jpg" style="margin: auto; width: 100%;"
+						<img
+							src="image.webp"
+							style="margin: auto; width: 100%;"
 							alt="image 1">
 						<p>The webinar will provide an overview of XNAT platform, which is
 							one of the most popular open-source software platforms for
@@ -79,76 +87,174 @@ $localizer = "../../";
 							preclinical image datasets, consequently providing a solid step
 							towards the adoption of the FAIR principles by the of our imaging
 							community.</p>
-
-						<h2>Speakers</h2>
-						<ul>
-							<li>Kranthi Thej Kandula Research Fellow at University of Turin
-								M.Sc. in computer science from Sapienza University</li>
-							<li>Francesco Gammaraccio Research Fellow at University of Turin
-								M.Sc. in biomedical engineering from Polytechnic University of
-								Turin.
-							
-							<li>Elisabetta Spinazzola Research Fellow at University of Turin
-								M.Sc. in biomedical engineering from Polytechnic University of
-								Turin</li>
-						</ul>
 						<h2>Details</h2>
 						<ul>
 							<li>Date: Thursday, June 15, 2023</li>
 							<li>Time: 9:30</li>
 							<li>How long: 3 hours</li>
 							<li>Language: English</li>
-							<li><a target="_blank"
-								href="https://docs.google.com/forms/d/e/1FAIpQLSeRmUl3DC0GRG6dnurp9P_5b9t7wSndOejHa3QI9MQWGlTREA/viewform?vc=0&c=0&w=1&flr=0"><span
-									style="font-weight: bold; color: #db6d16;">Register here!</span></a></li>
+							<!-- <li><a
+									target="_blank"
+									href="https://docs.google.com/forms/d/e/1FAIpQLSeRmUl3DC0GRG6dnurp9P_5b9t7wSndOejHa3QI9MQWGlTREA/viewform?vc=0&c=0&w=1&flr=0">
+									<span style="font-weight: bold; color: #db6d16;">Register here!</span>
+								</a></li> -->
+							<li><a
+									target="_blank"
+									href="https://youtube.com/playlist?list=PLW-oxncaXRqV0nsdKAiH6YL_gDHtbXJSE">
+									<span style="font-weight: bold; color: #db6d16;">Watch the
+										webinar!</span>
+								</a></li>
 						</ul>
 						<h2>Agenda</h2>
 						<h3 style="text-align: left;">SESSION 1:</h3>
 						<div style="margin-left: 20px;">
+							<ul>
+								<li>(9.30-9.35) <b>Introduction</b> - <i>Dario Longo (National
+										Research Council of Italy)</i></li>
+							</ul>
 							<h4>Theory</h4>
 							<ul>
-								<li>Biomedical Imaging Platform and Tools</li>
-								<li>XNAT for Preclinical Imaging Centers</li>
-								<li>Metadata management and storage</li>
+								<li>(9.35 - 10.00) <b>Biomedical Imaging Platform and Tools</b>
+									- <i><a href="mailto:kranthithej.kandula@unito.it">Kranthi They
+											Kandula</a> (University of Turin, Italy)</i>
+									<p>XNAT is an open-source imaging informatics platform with
+										many core features. In this presentation, we will explore the
+										potential of XNAT, including a brief but comprehensive guide
+										to its core features and to the OHIF Viewer.</p></li>
+								<li>(10.00 - 10.25) <b>Expanding XNAT capabilities to
+										Preclinical Imaging Centers</b> - <i><a
+											href="mailto:francesco.gammaraccio@unito.it">Francesco
+											Gammaraccio</a> (University of Turin, Italy)</i>
+									<p>We will present our tools to expand the basic functionality
+										of XNAT to preclinical imaging centers. We will introduce the
+										Windows desktop application that we developed to facilitate
+										the storage and the annotation of image datasets to the XNAT
+										imaging platform.</p></li>
+								<li>(10.25 - 10.50) <b>Metadata management and storage</b> - <i><a
+											href="mailto:elisabetta.spinazzola@unito.it">Elisabetta
+											Spinazzola</a> (University of Turin, Italy)</i>
+									<p>The aim of this lecture regards the exploitation of metadata
+										to curate image datasets, the adoption of the FAIR principles
+										and the exploitation of several ontologies to cover the
+										multidisciplinarity of the (preclinical) imaging field.</p></li>
 							</ul>
 						</div>
 						<h3 style="text-align: left;">SESSION 2:</h3>
 						<div style="margin-left: 20px;">
 							<h4>Pratical Demo</h4>
 							<ul>
-								<li>XNAT: Imaging informatics software platform</li>
-								<li>XNAT-PIC: Desktop application</li>
-								<li>Image processing on XNAT</li>
-								<li>PIDAR: Preclinical Image Dataset Repository</li>
+								<li>(10.55 - 11.20) <b>XNAT: Imaging informatics software
+										platform</b> - <i><a
+											href="mailto:kranthithej.kandula@unito.it">Kranthi They
+											Kandula</a> (University of Turin, Italy)</i>
+									<p>In this tutorial the basic functionalities of XNAT will be
+										shown, including how to login, create projects, add, explore
+										and share datasets. Moreover, we will see the Admin privileges
+										to extend XNAT core functionalities.</p></li>
+								<li>(11.20 - 11.45) <b>XNAT-PIC: XNAT for Preclinical Imaging
+										Centers</b> - <i><a
+											href="mailto:francesco.gammaraccio@unito.it">Francesco
+											Gammaraccio</a> (University of Turin, Italy)</i>
+									<p>This tutorial will explain how to install XNAT-PIC on your
+										computer and the main features of XNAT-PIC will be shown,
+										including the conversion of a whole dataset to the standard
+										DICOM, the annotation of single patients belonging to
+										different cohorts and the upload to the XNAT platform at
+										scan/session/patient level.</p></li>
+								<li>(11.45 - 12.10) <b>Image processing on XNAT using Docker</b>
+									- <i><a href="mailto:ferielromdhane@yahoo.fr">Feriel Ramdhane</a>
+										(Euro-BioImaging ERIC and National Research Council of Italy)</i>
+									<p>This demo will shows how to perform simple image processing
+										and image quality evaluation by exploiting docker containers,
+										including building a docker image with the image processing
+										code inside, setting up commands and how to lunch containers.
+									</p></li>
+								<li>(12.10 - 12.30) <b>PIDAR: Preclinical Image Dataset
+										Repository</b> - <i><a
+											href="mailto:elisabetta.spinazzola@unito.it">Elisabetta
+											Spinazzola</a> (University of Turin, Italy) </i>
+									<p>We will explore the developed Preclinical Image Dataset
+										Repository to store metadata information regarding preclinical
+										image datasets, the metadata model and the use of ontologies
+										to enrich the FAIRness and the value of the repository.</p></li>
 							</ul>
 						</div>
 						<h2>Contact us!</h2>
 						<ul>
-							<li><a href="mailto:kranthithej.kandula@unito.it">kranthithej.kandula@unito.it</a></li>
-							<li><a href="mailto:francesco.gammaraccio@unito.it">francesco.gammaraccio@unito.it</a></li>
-							<li><a href="mailto:elisabetta.spinazzola@unito.it">elisabetta.spinazzola@unito.it</a></li>
+							<li><a href="mailto:francesco.gammaraccio@unito.it">
+									<b>Francesco Gammaraccio</b><br>Research Fellow at University
+									of Turin - M.Sc. in biomedical engineering from Polytechnic
+									University of Turin
+								</a></li>
+							<li><a href="mailto:kranthithej.kandula@unito.it">
+									<b>Kranthi Thej Kandula</b><br>Research Fellow at University of
+									Turin - M.Sc. in computer science from Sapienza University
+								</a></li>
+							<li><a href="mailto:ferielromdhane@yahoo.fr">
+									<b>Feriel Ramdhane </b><br>Research Fellow at Euro-BioImaging
+									ERIC and National Research Council of Italy - PhD in Sciences
+									and Technologies of Engineering, National School of Engineering
+									of Tunis (Enit), University Al Manar of Tunis, Tunisia.
+								</a></li>
+							<li><a href="mailto:elisabetta.spinazzola@unito.it">
+									<b>Elisabetta Spinazzola</b><br>Research Fellow at University
+									of Turin - M.Sc. in biomedical engineering from Polytechnic
+									University of Turin
+								</a></li>
 						</ul>
 						<h2>Supported by:</h2>
 						<table class="supporters_table">
 							<tr>
-								<td class="supporters_table_cell"><a href="https://www.unito.it/"><img
-										alt="Unito logo" src="unito.png"></a></td>
-								<td class="supporters_table_cell"><a href="https://www.dbmss.unito.it/do/home.pl"><img
-										alt="DBMSS logo" src="DBMSS.png"></a></td>
-								<td class="supporters_table_cell"><a href="https://www.eurobioimaging.eu/"><img
-										alt="EuroBioImaging logo" src="eurobioimaging.png"></a></td>
-								<td class="supporters_table_cell"><a href="https://www.cnr.it/en"><img alt="CNR logo"
-										src="cnr.jpg"></a></td>
+								<td class="supporters_table_cell"><a
+										href="https://www.unito.it/">
+										<img
+											alt="Unito logo"
+											src="unito.webp">
+									</a></td>
+								<td class="supporters_table_cell"><a
+										href="https://www.dbmss.unito.it/do/home.pl">
+										<img
+											alt="DBMSS logo"
+											src="DBMSS.webp">
+									</a></td>
+								<td class="supporters_table_cell"><a
+										href="https://www.eurobioimaging.eu/">
+										<img
+											alt="EuroBioImaging logo"
+											src="eurobioimaging.webp">
+									</a></td>
+								<td class="supporters_table_cell"><a
+										href="https://www.cnr.it/en">
+										<img
+											alt="CNR logo"
+											src="cnr.webp">
+									</a></td>
 							</tr>
 							<tr>
-								<td class="supporters_table_cell"><a href="https://healthycloud.eu/"><img
-										alt="HealthyCloud logo" src="healthycloud.png"></a></td>
-								<td class="supporters_table_cell"><a href="https://www.eosc-life.eu/"><img
-										alt="eosc-life logo" src="eosc-life.png"></a></td>
-								<td class="supporters_table_cell"><a href="https://eosc4cancer.eu/"><img
-										alt="eosc-cancer logo" src="eosc-cancer.png"></a></td>
-								<td class="supporters_table_cell"><a href="https://european-union.europa.eu/index_en"><img alt="EU flag"
-										src="eu.jpg"></a></td>
+								<td class="supporters_table_cell"><a
+										href="https://healthycloud.eu/">
+										<img
+											alt="HealthyCloud logo"
+											src="healthycloud.webp">
+									</a></td>
+								<td class="supporters_table_cell"><a
+										href="https://www.eosc-life.eu/">
+										<img
+											alt="eosc-life logo"
+											src="eosc-life.webp">
+									</a></td>
+								<td class="supporters_table_cell"><a
+										href="https://eosc4cancer.eu/">
+										<img
+											alt="eosc-cancer logo"
+											src="eosc-cancer.webp">
+									</a></td>
+								<td class="supporters_table_cell"><a
+										href="https://european-union.europa.eu/index_en">
+										<img
+											alt="EU flag"
+											src="eu.webp">
+									</a></td>
 							</tr>
 						</table>
 					</div>
