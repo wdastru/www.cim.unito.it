66f0f63a110ed2db56a1402c5329a80c6ba5d9d6
Walter <walter@EPTADONE.>
2025-10-17 17:06:12 +0200

    fixed includes/home-side-nav.php

--- website/includes/home-side-nav.php
+++ website/includes/home-side-nav.php
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-<?php
-echo "
-        <div id='sidebar'>
-            <div class='padding'>
-                <dl id='list'>
-                    <dt class='subsection_link' id='section0-subsection1'>
-                        <a href='" . $localizer . "aboutus.php'>About Us</a>
-                    </dt> <dd></dd>
-                        <dt class='subsection_link' id='section0-subsection3'>
-                        <a href='" . $localizer . "where.php'>Where</a>
-                    </dt> <dd></dd>
-                    <dt class='subsection_link' id='section0-subsection4'>
-                        <a href='" . $localizer . "contacts.php'>Contacts</a>
-                    </dt> <dd></dd>
-                </dl>
-            </div>
-        </div>
-";
-?>              
-||||||| Stash base
 <?php
 echo "
         <div id='sidebar'>
@@ -40,28 +19,5 @@ echo "
             </div>
         </div>
 ";
-?>              
-=======
-<?php
-echo "
-        <div id='sidebar'>
-            <div class='padding'>
-                <dl id='list'>
-                    <dt class='subsection_link' id='section0-subsection1'>
-                        <a href='" . $localizer . "aboutus.php'>About Us</a>
-                    </dt>
-                    <dt class='subsection_link' id='section0-subsection2'>
-                        <a href='" . $localizer . "staff.php'>Staff</a>
-                    </dt>
-                        <dt class='subsection_link' id='section0-subsection3'>
-                        <a href='" . $localizer . "where.php'>Where</a>
-                    </dt>
-                    <dt class='subsection_link' id='section0-subsection4'>
-                        <a href='" . $localizer . "contacts.php'>Contacts</a>
-                    </dt>
-                </dl>
-            </div>
-        </div>
-";
-?>              
->>>>>>> Stashed changes
+?>
+
