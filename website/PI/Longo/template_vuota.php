65cef1615d7141a29cd4090b1895a36405f64968
Walter Dastrù <walter.dastru@unito.it>
2025-10-28 14:56:30 +0100

    fixed

--- website/PI/Longo/template_vuota.php
+++ website/PI/Longo/template_vuota.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 	$localizer = "../../";
 ?>
@@ -11,7 +10,7 @@
 <html lang="en">
 	<head>
 		<title>Contact Us | Longo Lab</title>
-	<?php require ($localizer . 'includes/head_const.inc.php'); ?>
+	<?php require ($localizer . 'includes/head_const_new.inc.php'); ?>
 	<?php require ($localizer . 'PI/Longo/includes/head_const_longo.php'); ?>
 	</head>
 	<body class="homepage">
@@ -25,7 +24,7 @@
 			<div class="unito-logo-container">                   
 	          </div></a>
 	          
-	          <a href="http://www.cim.unito.it" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
+	          <a href="http://www.cim.unito.it/website/" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
 	          <div class="cim-logo-container">
 	         </div></a>
 			
@@ -81,177 +80,3 @@
 			<script src="assets/js/main.js"></script>
 
 	</body>
-||||||| Stash base
-<!DOCTYPE HTML>
-<!--
-	ZeroFour by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Contact Us | Longo Lab</title>
-		<meta name="MobileOptimized" content="width">
-    <meta name="HandheldFriendly" content="true">
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-	</head>
-	<body class="homepage">
-		<div id="page-wrapper">
-
-			<!-- Header -->
-		<div id="header-wrapper">
-			
-			<!-- logo unito -->
-			<a href="http://www.unito.it" title="University of Torino" target="_blank" id="unito-logo" style="text-decoration: none;">
-			<div class="unito-logo-container">                   
-	          </div></a>
-	          
-	          <a href="http://www.cim.unito.it" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
-	          <div class="cim-logo-container">
-	         </div></a>
-			
-			
-			<div class="container">
-					
-				<!-- Header -->
-				<header id="header" style="margin: -30px 0 30px 0;">
-					<div class="inner">
-					
-						<!-- Logo -->
-						<h1>
-							<a href="home.php" id="logo">DARIO LONGO LAB</a>
-							<div>
-								<p style="color: gray; font-size=12px;" >Imaging Tumor Microenvironment</p>
-
-							</div>
-						</h1>
-						<!-- Nav -->
-						<nav id="nav">
-							<?php require ($localizer . 'PI/Longo/includes/menu_longo.php'); ?>
-						</nav>
-
-					</div>
-				</header>
-
-			</div>
-		</div>
-
-			<!-- Main Wrapper -->
-				<div id="main-wrapper">
-					<div class="wrapper style3">
-						<div class="inner">
-							<div class="container">
-					
-					
-							</div>
-						</div>
-					</div>
-				</div>
-
-		
-		</div>
-
-		<!-- Scripts -->
-
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/skel-viewport.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
-
-	</body>
-=======
-<!DOCTYPE HTML>
-<!--
-	ZeroFour by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Contact Us | Longo Lab</title>
-		<meta name="MobileOptimized" content="width">
-    <meta name="HandheldFriendly" content="true">
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-	</head>
-	<body class="homepage">
-		<div id="page-wrapper">
-
-			<!-- Header -->
-		<div id="header-wrapper">
-			
-			<!-- logo unito -->
-			<a href="http://www.unito.it" title="University of Torino" target="_blank" id="unito-logo" style="text-decoration: none;">
-			<div class="unito-logo-container">                   
-	          </div></a>
-	          
-	          <a href="http://www.cim.unito.it" title="Molecular Imaging Center" target="_blank" style="text-decoration: none;" >
-	          <div class="cim-logo-container">
-	         </div></a>
-			
-			
-			<div class="container">
-					
-				<!-- Header -->
-				<header id="header" style="margin: -30px 0 30px 0;">
-					<div class="inner">
-					
-						<!-- Logo -->
-						<h1>
-							<a href="home.php" id="logo">DARIO LONGO LAB</a>
-							<div>
-								<p style="color: gray; font-size=12px;" >Imaging Tumor Microenvironment</p>
-
-							</div>
-						</h1>
-						<!-- Nav -->
-						<nav id="nav">
-							<?php require ($localizer . 'PI/Longo/includes/menu_longo.php'); ?>
-						</nav>
-
-					</div>
-				</header>
-
-			</div>
-		</div>
-
-			<!-- Main Wrapper -->
-				<div id="main-wrapper">
-					<div class="wrapper style3">
-						<div class="inner">
-							<div class="container">
-					
-					
-							</div>
-						</div>
-					</div>
-				</div>
-
-		
-		</div>
-
-		<!-- Scripts -->
-
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/skel-viewport.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
-
-	</body>
->>>>>>> Stashed changes
-</html>
