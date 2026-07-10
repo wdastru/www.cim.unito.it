69e38b16a90377636cfd4f819b0ab239d17e144d
Walter Dastrù <walter.dastru@gmail.com>
2026-07-10 16:24:05 +0200

    main-menu ok

--- website/includes/home-side-nav-mobile.php
+++ website/includes/home-side-nav-mobile.php
@@ -1,11 +1,14 @@
 <div id="sidebar-mobile">
-    <input class="menu-btn" type="checkbox" id="sidebar-menu-btn" />
-    <label class="menu-icon" for="sidebar-menu-btn"
-        ><span class="navicon"></span
-    ></label>
-    <ul class="menu">
+
+    <button class="sidebar-toggle">
+        HOME ▼
+    </button>
+
+    <ul class="sidebar-mobile-list">
         <li><a href="<?php echo SITE_ROOT; ?>aboutus.php">About Us</a></li>
+        <li><a href="<?php echo SITE_ROOT; ?>people.php">Staff</a></li>
         <li><a href="<?php echo SITE_ROOT; ?>where.php">Where</a></li>
         <li><a href="<?php echo SITE_ROOT; ?>contacts.php">Contacts</a></li>
     </ul>
+
 </div>
--- website/includes/menu.php
+++ website/includes/menu.php
@@ -1,20 +1,21 @@
 <?php
-$base = SITE_ROOT;
-$menu = [
-    $base . 'index.php' => 'HOME',
-    $base . 'people.php' => 'PEOPLE',
-    $base . 'research.php' => 'RESEARCH',
-    $base . 'events.php' => 'EVENTS',
-    $base . 'facilities.php' => 'FACILITIES',
-    $base . 'partners.php' => 'PARTNERS',
-    $base . 'projects.php' => 'PROJECTS',
-    $base . 'publications.php' => 'PUBLICATIONS'
-];
+    $base = SITE_ROOT;
+    $menu = [
+    $base . 'index.php'        => 'HOME',
+    $base . 'people.php'       => 'PEOPLE',
+    $base . 'research.php'     => 'RESEARCH',
+    $base . 'events.php'       => 'EVENTS',
+    $base . 'facilities.php'   => 'FACILITIES',
+    $base . 'partners.php'     => 'PARTNERS',
+    $base . 'projects.php'     => 'PROJECTS',
+    $base . 'publications.php' => 'PUBLICATIONS',
+    ];
 ?>
-<nav class="menu">
-    <ul>
+<nav class="main-menu">
+    <button class="main-menu-toggle" aria-label="Open menu"></button>
+    <ul class="main-menu-list">
         <?php foreach ($menu as $url => $label): ?>
-            <li><a href="<?= $url ?>"><?= $label ?></a></li>
+            <li><a href="<?php echo $url ?>"><?php echo $label ?></a></li>
         <?php endforeach; ?>
     </ul>
 </nav>
